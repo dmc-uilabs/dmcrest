bc35a56835a44245f4721c8f42f97b014f029bc8
Cyprien Mvuanda <cyprien.mvuanda@turner.com>
2016-01-06 16:59:49 -0500

    Implementing SwaggerConfig.java, pulling configurations from Config.java

--- src/main/java/vehicle_forge/Config.java
+++ src/main/java/vehicle_forge/Config.java
@@ -2,15 +2,23 @@ package vehicle_forge;
 
 public class Config {
 
+	// DB
 	public static final String DB_PORT = System.getenv("DBport");
-
 	public static final String DB_HOST = "jdbc:postgresql://" + System.getenv("DBip");
- 		
-        public static final String DB_NAME = "gforge";
+    public static final String DB_NAME = "gforge";
  	public static final String DB_USER = System.getenv("DBuser");
  	public static final String DB_PASS = System.getenv("DBpass");
+ 	
+    // Documentation
+    public static final String DOC_API_NAME = "DMC Rest Services";
+    public static final String DOC_API_DESCRIPTION = "Serves the DMC Frontend Application";
+    public static final String DOC_API_VERSION = "0.1.0";
+    public static final String DOC_API_URL = "http://terms-of-service.url";
+    public static final String DOC_API_CONTACT = "Contact Name <contact@contact.com>";
+    public static final String DOC_API_LICENSE = "License";
+    public static final String DOC_API_LICENSE_URL = "http://licenseurl";
 
-	//LOGGING
+	//Loging
  	public static final String LOG_FILE = "logs/dmc_site_services.log";
     public static final boolean CONSOLE_LOGGING = true;
 }
---
+++ src/main/java/vehicle_forge/SharedAttributes/SwaggerConfig.java
@@ -0,0 +1,119 @@
+package vehicle_forge;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import static com.google.common.collect.Lists.newArrayList;
+import static springfox.documentation.schema.AlternateTypeRules.newRule;
+ 
+import java.time.LocalDate;
+import java.util.List;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.async.DeferredResult;
+ 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.schema.WildcardType;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.service.ApiInfo;
+
+import com.fasterxml.classmate.TypeResolver;
+
+@SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackageClasses = { ServiceController.class })
+public class SwaggerConfig {
+
+	@Bean
+	public Docket dmcRestApi() {
+		ApiInfo info = new ApiInfo(
+				Config.DOC_API_NAME, 
+				Config.DOC_API_DESCRIPTION,
+				Config.DOC_API_VERSION, 
+				Config.DOC_API_URL,
+				Config.DOC_API_CONTACT, 
+				Config.DOC_API_LICENSE, 
+				Config.DOC_API_LICENSE_URL
+		);
+	 
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(info)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build()
+				.pathMapping("/")
+				.directModelSubstitute(LocalDate.class, String.class)
+				.genericModelSubstitutes(ResponseEntity.class)
+				.alternateTypeRules(newRule(
+						typeResolver.resolve(DeferredResult.class,
+								typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
+						typeResolver.resolve(WildcardType.class)))
+				/*		
+				.globalResponseMessage(RequestMethod.GET,
+						newArrayList(new ResponseMessageBuilder()
+						.code(500).message("Server Error")
+								.responseModel(new ModelRef("Error"))
+								.build()
+						)
+				 )
+				 */
+				.securitySchemes(newArrayList(apiKey()))
+				.securityContexts(newArrayList(securityContext()));
+	}
+ 
+	@Autowired
+	private TypeResolver typeResolver;
+ 
+	private ApiKey apiKey() {
+		return new ApiKey(
+				"mykey", 
+				"api_key", 
+				"header"
+		);
+	}
+ 
+	private SecurityContext securityContext() {
+		return SecurityContext
+				.builder()
+				.securityReferences(defaultAuth())
+				.forPaths(PathSelectors.regex("/anyPath.*"))
+				.build();
+	}
+ 
+	List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return newArrayList(new SecurityReference("mykey", authorizationScopes));
+	}
+ 
+	@Bean
+	SecurityConfiguration security() {
+		return new SecurityConfiguration(
+				"test-app-client-id", 
+				"test-app-realm", 
+				"test-app", 
+				"apiKey"
+		);
+	}
+ 
+	@Bean
+	UiConfiguration uiConfig() {
+		return new UiConfiguration("validatorUrl");
+	}
+
+}
