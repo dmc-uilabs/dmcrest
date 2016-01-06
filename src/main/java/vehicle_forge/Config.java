c4e12f87290a6a215f346ad5c64ec52901d4808c
Jerome Francis <jerome.francis@ge.com>
2016-01-06 11:20:45 -0500

    edited Config and CORSfilter to be universally accessible

--- src/main/java/vehicle_forge/Config.java
+++ src/main/java/vehicle_forge/Config.java
@@ -2,13 +2,13 @@ package vehicle_forge;
 
 public class Config {
 
-	public static final String DB_PORT = "5432";
+	public static final String DB_PORT = System.getenv("DBport");
 
-	public static final String DB_HOST = "jdbc:postgresql://ec2-54-173-180-180.compute-1.amazonaws.com";
+	public static final String DB_HOST = "jdbc:postgresql://" + System.getenv("DBip");
  		
         public static final String DB_NAME = "gforge";
- 	public static final String DB_USER = "gforge";
- 	public static final String DB_PASS = "gforge";
+ 	public static final String DB_USER = System.getenv("DBuser");
+ 	public static final String DB_PASS = System.getenv("DBpass");
 
 	//LOGGING
  	public static final String LOG_FILE = "logs/dmc_site_services.log";
--- src/main/java/vehicle_forge/SharedAttributes/CORSFilter.java
+++ src/main/java/vehicle_forge/SharedAttributes/CORSFilter.java
@@ -15,7 +15,7 @@ public class CORSFilter implements Filter {
 
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
-		response.setHeader("Access-Control-Allow-Origin", "http://dmc-dev.geinspire.com");
+		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
