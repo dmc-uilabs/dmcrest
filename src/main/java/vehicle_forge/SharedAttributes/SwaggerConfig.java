e214817bec3f6dcd07d4774324217ee0d358730d
Cyprien Mvuanda <cyprien.mvuanda@turner.com>
2016-01-07 21:44:37 -0500

    Adding a resource handler for /swagger-ui.html

--- src/main/java/vehicle_forge/SharedAttributes/SwaggerConfig.java
+++ src/main/java/vehicle_forge/SharedAttributes/SwaggerConfig.java
@@ -32,11 +32,24 @@ import springfox.documentation.service.ApiInfo;
 
 import com.fasterxml.classmate.TypeResolver;
 
-@SpringBootApplication
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
 @EnableSwagger2
 @ComponentScan(basePackageClasses = { ServiceController.class })
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurerAdapter {
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+    
 	@Bean
 	public Docket dmcRestApi() {
 		ApiInfo info = new ApiInfo(
