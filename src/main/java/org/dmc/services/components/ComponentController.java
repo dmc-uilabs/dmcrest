b9a3eb467080c08b71d649ba4a66cfcff0894f87
Yan <212417509@logon.ds.ge.com>
2016-04-07 15:52:36 -0400

    add POST method to /components and commented GET method cause there is on POST for path /components in yaml

--- src/main/java/org/dmc/services/components/ComponentController.java
+++ src/main/java/org/dmc/services/components/ComponentController.java
@@ -4,7 +4,10 @@ import org.dmc.services.ServiceLogger;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -13,69 +16,80 @@ import java.util.ArrayList;
 public class ComponentController {
 
 	private final String logTag = ComponentController.class.getName();
-	
-	private ComponentDao component = new ComponentDao(); 
-    @RequestMapping(value = "/components/{componentID}", method = RequestMethod.GET)
-    public Component getComponent(@PathVariable("componentID") int componentID) {
-    	ServiceLogger.log(logTag, "In getComponent");
-    	ServiceLogger.log(logTag, "In getComponent, componentID: " + componentID);
-    	return component.getComponent(componentID);
-    }
-    
-	private ComponentListDao componentList = new ComponentListDao(); 
-    @RequestMapping(value = "/components", method = RequestMethod.GET)
-    public ArrayList<Component> getComponentList() {
-    	ServiceLogger.log(logTag, "In getcomponentList");/*
-    	ServiceLogger.log(logTag, "In getcomponentList, authorization: " + authorization);*/
-    	return componentList.getComponentList();
-    	
-    }
-   
-    @RequestMapping(value = "/projects/{projectId}/components", method = RequestMethod.GET)
-    public ArrayList<Component> getComponentList(@PathVariable("projectId") int projectId){
-    	ServiceLogger.log(logTag, "Getting components of project " + projectId);
-    	return componentList.getComponentList(projectId);
-    }
-    
-    /*
-    @RequestMapping(value = "/role/create", method = RequestMethod.POST)
-    @ResponseBody
-    public String createRole(@RequestParam(value="name", defaultValue="anonymous") String name) {
-    	System.out.println("In createRole role: " + name);
-    	
-    	
-    	//RoleDao.createRole creates a new Role in the database using the provided POST params
-    	//it instantiates a new role with these params like i.e new Role(param.name, param.title.....)
-    	//this controller in turn returns this new Role instance to the reques using spring's Jackson which
-    	//converts the response to JSON
-    	
-    	return RoleDao.createRole(params);
-    	
-    	//Create role and update db through JDBC then return role using new role's id
-    	
-    	if (true) {
-    		return "Could not create role";
-    		//return new ResponseEntity<Role>(HttpStatus.BAD_REQUEST);
-    	}
-    	//return new ResponseEntity<String>(json,HttpStatus.OK);
-    	return "Role created successfully";
-    	
-    }
-*/
-    
-    /*
-    @RequestMapping(value = "/role/update", method = RequestMethod.POST)
-    @ResponseBody
-    public String updateRole(@RequestParam(value="id", defaultValue="-1") int id) {
-    	System.out.println("In createRole role: " + id);
-    	
-    	
-    	//RoleDao.createRole updates the Role in the database identified by id using the provided POST params
-    	//it creates an instance of this role i.e new Role(param.id, param.name, param.title.....)
-    	//this controller in turn returns this updated Role instance to the reques using spring's Jackson which
-    	//converts the response to JSON
-    	
-    	return RoleDao.updateRole(params);
-    }
-    */
+
+	@RequestMapping(value = "/components", produces = { "application/json", "text/html" }, method = RequestMethod.POST)
+	public ResponseEntity<Void> postComponent(@RequestBody Component body) {
+		// do some magic!
+		return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+	}
+
+	private ComponentDao component = new ComponentDao();
+
+	@RequestMapping(value = "/components/{componentID}", method = RequestMethod.GET)
+	public Component getComponent(@PathVariable("componentID") int componentID) {
+		ServiceLogger.log(logTag, "In getComponent");
+		ServiceLogger.log(logTag, "In getComponent, componentID: " + componentID);
+		return component.getComponent(componentID);
+	}
+
+	private ComponentListDao componentList = new ComponentListDao();
+	/*
+	 * @RequestMapping(value = "/components", method = RequestMethod.GET) public
+	 * ArrayList<Component> getComponentList() { ServiceLogger.log(logTag,
+	 * "In getcomponentList"); ServiceLogger.log(logTag,
+	 * "In getcomponentList, authorization: " + authorization); return
+	 * componentList.getComponentList();
+	 * 
+	 * }
+	 */
+
+	@RequestMapping(value = "/projects/{projectId}/components", method = RequestMethod.GET)
+	public ArrayList<Component> getComponentList(@PathVariable("projectId") int projectId) {
+		ServiceLogger.log(logTag, "Getting components of project " + projectId);
+		return componentList.getComponentList(projectId);
+	}
+
+	/*
+	 * @RequestMapping(value = "/role/create", method = RequestMethod.POST)
+	 * 
+	 * @ResponseBody public String createRole(@RequestParam(value="name",
+	 * defaultValue="anonymous") String name) { System.out.println(
+	 * "In createRole role: " + name);
+	 * 
+	 * 
+	 * //RoleDao.createRole creates a new Role in the database using the
+	 * provided POST params //it instantiates a new role with these params like
+	 * i.e new Role(param.name, param.title.....) //this controller in turn
+	 * returns this new Role instance to the reques using spring's Jackson which
+	 * //converts the response to JSON
+	 * 
+	 * return RoleDao.createRole(params);
+	 * 
+	 * //Create role and update db through JDBC then return role using new
+	 * role's id
+	 * 
+	 * if (true) { return "Could not create role"; //return new
+	 * ResponseEntity<Role>(HttpStatus.BAD_REQUEST); } //return new
+	 * ResponseEntity<String>(json,HttpStatus.OK); return
+	 * "Role created successfully";
+	 * 
+	 * }
+	 */
+
+	/*
+	 * @RequestMapping(value = "/role/update", method = RequestMethod.POST)
+	 * 
+	 * @ResponseBody public String updateRole(@RequestParam(value="id",
+	 * defaultValue="-1") int id) { System.out.println("In createRole role: " +
+	 * id);
+	 * 
+	 * 
+	 * //RoleDao.createRole updates the Role in the database identified by id
+	 * using the provided POST params //it creates an instance of this role i.e
+	 * new Role(param.id, param.name, param.title.....) //this controller in
+	 * turn returns this updated Role instance to the reques using spring's
+	 * Jackson which //converts the response to JSON
+	 * 
+	 * return RoleDao.updateRole(params); }
+	 */
 }
--- src/test/java/org/dmc/services/ComponentIT.java
+++ src/test/java/org/dmc/services/ComponentIT.java
@@ -1,6 +1,7 @@
 package org.dmc.services;
 
 import org.junit.Test;
+import org.springframework.http.HttpStatus;
 
 import static com.jayway.restassured.RestAssured.*;
 
@@ -8,7 +9,7 @@ import static com.jayway.restassured.module.jsv.JsonSchemaValidator.matchesJsonS
 
 //@Ignore
 public class ComponentIT extends BaseIT {
-	
+	private String knownUserEPPN = "fforgeadmin";
     
 
 	@Test
@@ -20,11 +21,17 @@ public class ComponentIT extends BaseIT {
         body(matchesJsonSchemaInClasspath("Schemas/componentSchema.json")); 
 	}
 	
-	@Test
+	/*@Test
 	public void testComponentList(){
 		expect().statusCode(200).when().get("/components").then().
         body(matchesJsonSchemaInClasspath("Schemas/componentListSchema.json"));
+	}*/
+	
+	@Test
+	public void testComponents(){
+		given().header("AJP_eppn", knownUserEPPN).expect().statusCode(405).when().get("/components");
 	}
+
 	
 	@Test
 	public void testProjectComponents(){
