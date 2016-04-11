be60a91c8550471d9ef3ba4d3160117d3a5d0333
DanYang <yangd@ge.com>
2016-04-11 16:16:46 -0400

    Code clean, and added change log

--- src/main/java/org/dmc/services/company/CompanySkill.java
+++ src/main/java/org/dmc/services/company/CompanySkill.java
@@ -2,7 +2,6 @@ package org.dmc.services.company;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.Objects;
 
 public class CompanySkill  {
   
@@ -43,36 +42,4 @@ public class CompanySkill  {
     this.name = name;
   }
 
-  
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    CompanySkill companySkill = (CompanySkill) o;
-    return Objects.equals(this.id, companySkill.id) &&
-        Objects.equals(this.companyId, companySkill.companyId) &&
-        Objects.equals(this.name, companySkill.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(this.id, this.companyId, this.name);
-  }
-
-  @Override
-  public String toString()  {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class CompanySkill {\n");
-    
-    sb.append("  id: ").append(this.id).append("\n");
-    sb.append("  companyId: ").append(this.companyId).append("\n");
-    sb.append("  name: ").append(this.name).append("\n");
-    sb.append("}\n");
-    return sb.toString();
-  }
 }
--- src/main/java/org/dmc/services/company/CompanySkillController.java
+++ src/main/java/org/dmc/services/company/CompanySkillController.java
@@ -23,55 +23,60 @@ import java.util.ArrayList;
 import javax.xml.ws.http.HTTPException;
 
 import static org.springframework.http.MediaType.*;
+
 @RestController
 public class CompanySkillController {
 	private CompanySkillDao skillDao = new CompanySkillDao();
 	private final String logTag = CompanySkillController.class.getName();
-	
-	@RequestMapping(value = "/company_skills", produces = { "application/json", "text/html" }, consumes = { "application/json",
-			"text/xml" }, method = RequestMethod.POST)
-	public ResponseEntity<?> companySkillsPost(@RequestBody CompanySkill companySkill,
-			@RequestHeader(value = "AJP_eppn", defaultValue = "testUser") String userEPPN) {
-		int statusCode = HttpStatus.OK.value();
 
+	@RequestMapping(value = "/company_skills", produces = { "application/json",
+			"text/html" }, consumes = { "application/json", "text/xml" }, method = RequestMethod.POST)
+	public ResponseEntity<?> companySkillsPost(
+			@RequestBody CompanySkill companySkill,
+			@RequestHeader(value = "AJP_eppn", defaultValue = "testUser") String userEPPN) {
 		try {
 			ServiceLogger.log(this.logTag, "Before create entry");
-			CompanySkill ck = this.skillDao.createCompanySkill(companySkill, userEPPN);
+			CompanySkill ck = this.skillDao.createCompanySkill(companySkill,
+					userEPPN);
 			ServiceLogger.log(this.logTag, "After create entry");
-			return new ResponseEntity<CompanySkill>(
-					HttpStatus.OK);
-
-		}
-		catch (DMCServiceException e) {
+			return new ResponseEntity<CompanySkill>(HttpStatus.OK);
+		} catch (DMCServiceException e) {
 			ServiceLogger.log(this.logTag, "Exception:" + e.getErrorMessage());
 			HttpHeaders headers = new HttpHeaders();
-			return new ResponseEntity<String>("DMCServerException:" + e.getErrorMessage(),headers,HttpStatus.BAD_REQUEST );
-		}
-		catch (Exception e) {
-			ServiceLogger.log(this.logTag, "Exception:" + e.getMessage());
-			return new ResponseEntity<CompanySkill>(companySkill, HttpStatus.BAD_REQUEST);
-		}
-	}
-    
-	@RequestMapping(value = "/company_skills/{skillID}", produces = { "application/json", "text/html" }, method = RequestMethod.DELETE)
-	public ResponseEntity<String> companySkillDelete(@PathVariable("skillID") String skillID, @RequestHeader(value = "AJP_eppn", defaultValue = "testUser")String userEPPN) 
-			 {
-		ServiceLogger.log(this.logTag, "User: " + userEPPN + " is trying to delete:" + skillID);
-		try {
-			int status = this.skillDao.deleteCompanySkills(skillID, userEPPN);
-			ServiceLogger.log(this.logTag, "User: " + userEPPN + " finish deleting:" + skillID + "with Status:" + status);
-			return new ResponseEntity<String>("Delete succeeded.", HttpStatus.OK);
+			return new ResponseEntity<String>("DMCServerException:"
+					+ e.getErrorMessage(), headers, HttpStatus.BAD_REQUEST);
 		} catch (Exception e) {
 			ServiceLogger.log(this.logTag, "Exception:" + e.getMessage());
-			//return new ResponseEntity<String>(HttpStatus.valueOf(e.getStatusCode()));
-			return new ResponseEntity<String>(e.getMessage(),HttpStatus.BAD_REQUEST);
-		}		
+			return new ResponseEntity<CompanySkill>(companySkill,
+					HttpStatus.BAD_REQUEST);
+		}
 	}
 
-	@RequestMapping(value = "/companies/{companyID}/company_skills", method =
-	RequestMethod.GET)
-	public ArrayList<CompanySkill> getCompanySkills(@PathVariable("companyID") int companyID,
-			@RequestHeader(value = "AJP_eppn", defaultValue = "testUser") String userEPPN)  throws Exception{
+	@RequestMapping(value = "/company_skills/{skillID}", produces = {
+			"application/json", "text/html" }, method = RequestMethod.DELETE)
+	public ResponseEntity<String> companySkillDelete(
+			@PathVariable("skillID") String skillID,
+			@RequestHeader(value = "AJP_eppn", defaultValue = "testUser") String userEPPN) {
+		try {
+			int status = this.skillDao.deleteCompanySkills(skillID, userEPPN);
+			ServiceLogger.log(this.logTag, "User: " + userEPPN
+					+ " deleted:" + skillID + "with Status:" + status);
+			return new ResponseEntity<String>("Delete succeeded.",
+					HttpStatus.OK);
+		} catch (Exception e) {
+			ServiceLogger.log(this.logTag, "Exception:" + e.getMessage());
+			// return new
+			// ResponseEntity<String>(HttpStatus.valueOf(e.getStatusCode()));
+			return new ResponseEntity<String>(e.getMessage(),
+					HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@RequestMapping(value = "/companies/{companyID}/company_skills", method = RequestMethod.GET)
+	public ArrayList<CompanySkill> getCompanySkills(
+			@PathVariable("companyID") int companyID,
+			@RequestHeader(value = "AJP_eppn", defaultValue = "testUser") String userEPPN)
+			throws Exception {
 
 		return this.skillDao.getCompanySkills(userEPPN, companyID);
 	}
