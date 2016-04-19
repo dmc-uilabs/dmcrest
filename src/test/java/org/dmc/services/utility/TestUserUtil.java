175c51b54d2cf3081191a7923937c7fc9be53575
Ben Beckmann <beckmann@ge.com>
2016-04-19 11:24:27 -0400

    Added /account_servers POST endpoint and test

--- src/test/java/org/dmc/services/utility/TestUserUtil.java
+++ src/test/java/org/dmc/services/utility/TestUserUtil.java
@@ -4,6 +4,9 @@ import java.util.Date;
 import java.util.UUID;
 import java.text.SimpleDateFormat;
 
+import static com.jayway.restassured.RestAssured.*;
+
+
 public class TestUserUtil {
     public static String generateTime() {
         Date date = new Date();
@@ -21,4 +24,27 @@ public class TestUserUtil {
         UUID uniqueID = UUID.randomUUID();
         return "uniqueUser" + uniqueID.toString();
     }
+	
+	
+	public static String createNewUser() {
+        String unique = TestUserUtil.generateTime();
+        
+        Integer id =
+        given().
+        header("Content-type", "text/plain").
+        header("AJP_eppn", "userEPPN" + unique).
+        header("AJP_givenName", "userGivenName" + unique).
+        header("AJP_sn", "userSurname" + unique).
+        header("AJP_displayName", "userDisplayName" + unique).
+        header("AJP_mail", "userEmail" + unique).
+        expect().
+        statusCode(200).
+		when().
+        post("/users/create").
+		then().
+        extract().path("id");
+        
+        return new String("userEPPN" + unique);
+    }
+
 }
