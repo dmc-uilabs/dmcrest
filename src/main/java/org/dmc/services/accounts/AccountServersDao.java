6c8c75f1c37b9beadf1f93c413daa9384454082f
Ben Beckmann <beckmann@ge.com>
2016-04-26 13:46:23 -0400

    Fixed formatting.

--- src/main/java/org/dmc/services/accounts/AccountServersDao.java
+++ src/main/java/org/dmc/services/accounts/AccountServersDao.java
@@ -72,7 +72,7 @@ class AccountServersDao {
 	/**
      Patch a record for an existing server registered by a user
      @param UserAccountServer - information of server being patched
-	 @param String - user unique user name
+     @param String - user unique user name
      @return UserAccountServer
      @throws HTTPException
      **/
@@ -81,7 +81,7 @@ class AccountServersDao {
 		int serverId = Integer.parseInt(serverIdStr);
 		Util util = Util.getInstance();
 		
-        ServiceLogger.log(logTag, "In patchUserAccountServer for userEPPN: " + userEPPN);
+		ServiceLogger.log(logTag, "In patchUserAccountServer for userEPPN: " + userEPPN);
         
 		user_id_lookedup = getAutorizedUserId(userAccountServer, userEPPN);
         
@@ -92,126 +92,121 @@ class AccountServersDao {
 			throw new HTTPException(HttpStatus.UNAUTHORIZED.value());
 		}
 		
-        try {
-            // update user's record in users table
-            String createUserAccountServerQuery = "UPDATE servers SET url = ?, alias = ? WHERE user_id = ? AND server_id = ?";  // ToDo: update status
+		try {
+		    // update user's record in users table
+		    String createUserAccountServerQuery = "UPDATE servers SET url = ?, alias = ? WHERE user_id = ? AND server_id = ?";  // ToDo: update status
 			
-            PreparedStatement preparedStatement = DBConnector.prepareStatement(createUserAccountServerQuery);
-            preparedStatement.setString(1, userAccountServer.getIp());
-            preparedStatement.setString(2, userAccountServer.getName());
-            preparedStatement.setInt(3, user_id_lookedup);
-            preparedStatement.setInt(4, Integer.parseInt(userAccountServer.getId()));
-            // ToDo: need to add status update
+		    PreparedStatement preparedStatement = DBConnector.prepareStatement(createUserAccountServerQuery);
+		    preparedStatement.setString(1, userAccountServer.getIp());
+		    preparedStatement.setString(2, userAccountServer.getName());
+		    preparedStatement.setInt(3, user_id_lookedup);
+		    preparedStatement.setInt(4, Integer.parseInt(userAccountServer.getId()));
+		    // ToDo: need to add status update
 			
-			if(preparedStatement.executeUpdate() != 1) {
-                throw new SQLException("Unable to update servers" +
-                                       " for user_id: " + user_id_lookedup + " to " + userAccountServer.toString());
-            }
-			
-        } catch (SQLException e) {
-			ServiceLogger.log(logTag, e.getMessage());
-            throw new HTTPException(HttpStatus.UNAUTHORIZED.value()); // ToDo need to determine what HTTP error to throw
+		    if(preparedStatement.executeUpdate() != 1) {
+			throw new SQLException("Unable to update servers" +
+					       " for user_id: " + user_id_lookedup + 
+					       " to " + userAccountServer.toString());
+		    }
+		} catch (SQLException e) {
+		    ServiceLogger.log(logTag, e.getMessage());
+		    throw new HTTPException(HttpStatus.UNAUTHORIZED.value()); // ToDo need to determine what HTTP error to throw
 		}
-				
-        return userAccountServer;
+		return userAccountServer;
 	}
 	
 	
-	
-	
 	public UserAccountServer getUserAccountServer(int serverID, String userEPPN) throws HTTPException {
-        ServiceLogger.log(logTag, "In getUserAccountServer for userEPPN: " + userEPPN + " and server id " + serverID);
+	    ServiceLogger.log(logTag, "In getUserAccountServer for userEPPN: " + userEPPN + " and server id " + serverID);
 
-		Util util = Util.getInstance();
-		UserAccountServer userAccountServer = new UserAccountServer();
-		int user_id_lookedup = -1;
+	    Util util = Util.getInstance();
+	    UserAccountServer userAccountServer = new UserAccountServer();
+	    int user_id_lookedup = -1;
 		
-		try{
-            user_id_lookedup = UserDao.getUserID(userEPPN);
-        } catch (SQLException e) {
-			ServiceLogger.log(logTag, e.getMessage());
-			throw new HTTPException(HttpStatus.UNAUTHORIZED.value()); // unknow user
-        }
+	    try{
+		user_id_lookedup = UserDao.getUserID(userEPPN);
+	    } catch (SQLException e) {
+	       	ServiceLogger.log(logTag, e.getMessage());
+	       	throw new HTTPException(HttpStatus.UNAUTHORIZED.value()); // unknow user
+	    }
 		
-		// update user's record in users table
-		String getUserAccountServerQuery = "SELECT * FROM servers WHERE user_id = ? AND server_id = ?";  // ToDo store status
+	    // update user's record in users table
+	    String getUserAccountServerQuery = "SELECT * FROM servers WHERE user_id = ? AND server_id = ?";  // ToDo store status
 			
-		PreparedStatement preparedStatement = DBConnector.prepareStatement(getUserAccountServerQuery);
-		try {
-			preparedStatement.setInt(1, user_id_lookedup);
-			preparedStatement.setInt(2, serverID);
+	    PreparedStatement preparedStatement = DBConnector.prepareStatement(getUserAccountServerQuery);
+	    try {
+	       	preparedStatement.setInt(1, user_id_lookedup);
+	       	preparedStatement.setInt(2, serverID);
 			
-			ResultSet resultSet = preparedStatement.executeQuery();
-			if(resultSet.next()) {
-				userAccountServer.setId(Integer.toString(resultSet.getInt("server_id")));
-				userAccountServer.setIp(resultSet.getString("url"));
-				userAccountServer.setAccountId(Integer.toString(resultSet.getInt("user_id")));
-				userAccountServer.setName(resultSet.getString("alias"));
-				// ToDo set status
-//				userAccountServer.setStatus();
-			} else {
-				throw new HTTPException(HttpStatus.UNAUTHORIZED.value()); // no result set returned
-			}
-		} catch (SQLException e) {
-			ServiceLogger.log(logTag, e.getMessage());
-			throw new HTTPException(HttpStatus.INTERNAL_SERVER_ERROR.value());
+	       	ResultSet resultSet = preparedStatement.executeQuery();
+	       	if(resultSet.next()) {
+		    userAccountServer.setId(Integer.toString(resultSet.getInt("server_id")));
+		    userAccountServer.setIp(resultSet.getString("url"));
+		    userAccountServer.setAccountId(Integer.toString(resultSet.getInt("user_id")));
+		    userAccountServer.setName(resultSet.getString("alias"));
+		    // ToDo set status
+//		     	userAccountServer.setStatus();
+		} else {
+		    throw new HTTPException(HttpStatus.UNAUTHORIZED.value()); // no result set returned
 		}
-		
-        return userAccountServer;
+	    } catch (SQLException e) {
+		ServiceLogger.log(logTag, e.getMessage());
+	       	throw new HTTPException(HttpStatus.INTERNAL_SERVER_ERROR.value());
+	    }
+	    return userAccountServer;
 	}
 
 	
 	
 	public void deleteUserAccountServer(int serverID, String userEPPN) throws HTTPException {
-        ServiceLogger.log(logTag, "In deleteUserAccountServer for userEPPN: " + userEPPN + " and server id " + serverID);
+	    ServiceLogger.log(logTag, "In deleteUserAccountServer for userEPPN: " + userEPPN + " and server id " + serverID);
+	     
+	    Util util = Util.getInstance();
+	    int user_id_lookedup = -1;
 		
-		Util util = Util.getInstance();
-		int user_id_lookedup = -1;
-		
-		try{
-            user_id_lookedup = UserDao.getUserID(userEPPN);
-        } catch (SQLException e) {
-			ServiceLogger.log(logTag, e.getMessage());
-			throw new HTTPException(HttpStatus.UNAUTHORIZED.value()); // unknow user
-        }
+	    try{
+		user_id_lookedup = UserDao.getUserID(userEPPN);
+	    } catch (SQLException e) {
+	    	ServiceLogger.log(logTag, e.getMessage());
+	       	throw new HTTPException(HttpStatus.UNAUTHORIZED.value()); // unknow user
+	    }
 				
-		// update user's record in users table
-		String deleteUserAccountServerQuery = "DELETE FROM servers WHERE user_id = ? AND server_id = ?";
+	    // update user's record in users table
+	    String deleteUserAccountServerQuery = "DELETE FROM servers WHERE user_id = ? AND server_id = ?";
 		
-		PreparedStatement preparedStatement = DBConnector.prepareStatement(deleteUserAccountServerQuery);
-		try {
-			preparedStatement.setInt(1, user_id_lookedup);
-			preparedStatement.setInt(2, serverID);
-			
-			if(preparedStatement.executeUpdate() != 1) {
-				throw new HTTPException(HttpStatus.INTERNAL_SERVER_ERROR.value()); // single item not deleted
-			}
-			
-		} catch (SQLException e) {
-			ServiceLogger.log(logTag, e.getMessage());
-			throw new HTTPException(HttpStatus.INTERNAL_SERVER_ERROR.value());
+	    PreparedStatement preparedStatement = DBConnector.prepareStatement(deleteUserAccountServerQuery);
+	    try {
+	       	preparedStatement.setInt(1, user_id_lookedup);
+	       	preparedStatement.setInt(2, serverID);
+	       	
+	       	if(preparedStatement.executeUpdate() != 1) {
+		    throw new HTTPException(HttpStatus.INTERNAL_SERVER_ERROR.value()); // single item not deleted
 		}
-		
-        return;
+			
+	    } catch (SQLException e) {
+	       	ServiceLogger.log(logTag, e.getMessage());
+	       	throw new HTTPException(HttpStatus.INTERNAL_SERVER_ERROR.value());
+	    }
+	    return;
 	}
 	
 	
 	private int getAutorizedUserId(UserAccountServer userAccountServer, String userEPPN) {
-		int user_id_lookedup = -1;
-		try{
-            user_id_lookedup = UserDao.getUserID(userEPPN);
-        } catch (SQLException e) {
-			ServiceLogger.log(logTag, e.getMessage());
-			throw new HTTPException(HttpStatus.UNAUTHORIZED.value()); // unknow user
-        }
+	    int user_id_lookedup = -1;
+	    
+	    try{
+		user_id_lookedup = UserDao.getUserID(userEPPN);
+	    } catch (SQLException e) {
+	       	ServiceLogger.log(logTag, e.getMessage());
+	       	throw new HTTPException(HttpStatus.UNAUTHORIZED.value()); // unknow user
+	    }
         
-        // check if user has permission to update account
-        if(user_id_lookedup != Integer.parseInt(userAccountServer.getAccountId())) {
-			ServiceLogger.log(logTag, "Looked up user id " + user_id_lookedup +
-							  " does not match account id of server " + userAccountServer.getAccountId());
-            throw new HTTPException(HttpStatus.UNAUTHORIZED.value()); // account id and user id do not match
-        }
-		return user_id_lookedup;
+	    // check if user has permission to update account
+	    if(user_id_lookedup != Integer.parseInt(userAccountServer.getAccountId())) {
+	       	ServiceLogger.log(logTag, "Looked up user id " + user_id_lookedup +
+       					  " does not match account id of server " + userAccountServer.getAccountId());
+		throw new HTTPException(HttpStatus.UNAUTHORIZED.value()); // account id and user id do not match
+	    }
+	    return user_id_lookedup;
 	}
-
 }
