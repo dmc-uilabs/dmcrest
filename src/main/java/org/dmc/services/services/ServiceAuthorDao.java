d2b224aebb7f7552820fe81b697107b7a9ef6e1e
cyptm <cyptm@hotmail.com>
2016-05-06 00:07:38 -0400

    [DMC-634] Created Service author POST and GET, tables pending

---
+++ src/main/java/org/dmc/services/services/ServiceAuthorDao.java
@@ -0,0 +1,139 @@
+package org.dmc.services.services;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+
+import org.dmc.services.Config;
+import org.dmc.services.DBConnector;
+import org.dmc.services.DMCError;
+import org.dmc.services.DMCServiceException;
+import org.dmc.services.ErrorMessage;
+import org.dmc.services.Id;
+import org.dmc.services.ServiceLogger;
+import org.dmc.services.company.CompanyVideo;
+import org.dmc.services.profile.Profile;
+import org.dmc.services.sharedattributes.FeatureImage;
+import org.dmc.services.sharedattributes.Util;
+import org.dmc.services.users.UserDao;
+import org.dmc.services.users.UserOnboardingDao;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+
+import javax.xml.ws.http.HTTPException;
+
+public class ServiceAuthorDao {
+
+	private final String logTag = ServiceAuthorDao.class.getName();
+	private ResultSet resultSet;
+	private Connection connection;
+
+	/**
+	 * Create Service Author
+	 * @param id
+	 * @param video
+	 * @param userEPPN
+	 * @return
+	 * @throws DMCServiceException
+	 */
+	public Id createServiceAuthor(ServiceAuthor author, String userEPPN) throws DMCServiceException {
+		
+		Util util = Util.getInstance();
+		PreparedStatement statement;
+		String query;
+		int authorId = -9999, userIdEPPN;
+
+		Connection connection = DBConnector.connection();
+		try {
+			connection.setAutoCommit(false);
+		} catch (SQLException ex) {
+			throw new DMCServiceException(DMCError.OtherSQLError, "An SQL exception has occured");
+		}
+
+		try {
+			
+			userIdEPPN = UserDao.getUserID(userEPPN);
+	
+			//create Service Author
+			query = "Insert INTO table (service_id, display_name, job_title, follow, avatar, company)"
+					+ "VALUES (?, ?, ?, ?, ?, ?)";
+			
+			statement = DBConnector.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+			statement.setInt(1, 0);
+			statement.setString(2, author.getServiceId());
+			statement.setString(3, author.getDisplayName());
+			statement.setString(3, author.getJobTitle());
+			statement.setBoolean(3, author.getFollow());
+			statement.setString(3, author.getAvatar());
+			statement.setString(3, author.getCompany());
+			statement.executeUpdate();
+			
+			authorId = util.getGeneratedKey(statement, "id");
+			ServiceLogger.log(logTag, "Created Author Id: " + authorId);
+
+			connection.commit();
+			
+		} catch (SQLException e) {
+			ServiceLogger.log(logTag, e.getMessage());
+			if (connection != null) {
+				try {
+					ServiceLogger.log(logTag, "Transaction createServiceAuthor Rolled back");
+					connection.rollback();
+				} catch (SQLException ex) {
+					ServiceLogger.log(logTag, ex.getMessage());
+				}
+			}
+			throw new DMCServiceException(DMCError.OtherSQLError, e.getMessage());
+		} finally {
+			if (connection != null) {
+				try {
+					connection.setAutoCommit(true);
+				} catch (SQLException ex) {}
+			}
+		}
+
+		return new Id.IdBuilder(authorId).build();
+	}
+	
+	public ArrayList<ServiceAuthor> getServiceAuthors(int serviceId, String userEPPN) throws DMCServiceException {
+		ArrayList<ServiceAuthor> authors = new ArrayList<ServiceAuthor>();
+		ServiceLogger.log(this.logTag, "User: " + userEPPN + " asking for all authors of the service: " + serviceId);
+		try {
+			String query = "SELECT * FROM table WHERE id = ?";
+
+			PreparedStatement preparedStatement = DBConnector.prepareStatement(query);
+			preparedStatement.setInt(1, serviceId);
+			this.resultSet = preparedStatement.executeQuery();
+
+			while (this.resultSet.next()) {
+				int id = this.resultSet.getInt("id");
+				String displayName = this.resultSet.getString("display_name");
+				String jobTitle = this.resultSet.getString("job_title");
+				Boolean follow = this.resultSet.getBoolean("follow");
+				String avatar = this.resultSet.getString("avatar");
+				String company = this.resultSet.getString("company");
+				ServiceAuthor author = new ServiceAuthor();
+				author.setId(String.valueOf(id));
+				author.setServiceId(String.valueOf(serviceId));
+				author.setJobTitle(jobTitle);
+				author.setFollow(follow);
+				author.setAvatar(avatar);
+				author.setCompany(company);
+				authors.add(author);
+			}
+		} catch (Exception e) {
+			throw new HTTPException(HttpStatus.INTERNAL_SERVER_ERROR.value());
+		}
+		return authors;
+	}
+
+}
--- src/main/java/org/dmc/services/services/ServiceAuthorsController.java
+++ src/main/java/org/dmc/services/services/ServiceAuthorsController.java
@@ -1,10 +1,19 @@
 package org.dmc.services.services;
 
+import java.util.ArrayList;
+
+import org.dmc.services.DMCServiceException;
+import org.dmc.services.Id;
+import org.dmc.services.ServiceLogger;
+import org.dmc.services.discussions.DiscussionListDao;
+import org.dmc.services.projects.Project;
+import org.dmc.services.projects.ProjectController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,6 +24,9 @@ import static org.springframework.http.MediaType.*;
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringMVCServerCodegen", date = "2016-04-07T17:42:57.404Z")
 public class ServiceAuthorsController {
 
+	private final String logTag = ServiceAuthorsController.class.getName();
+	private ServiceAuthorDao serviceAuthorDao = new ServiceAuthorDao();
+	
 	@RequestMapping(value = "/{autorhId}", produces = { "application/json",
 			"text/html" }, method = RequestMethod.DELETE)
 	public ResponseEntity<Void> serviceAuthorsAutorhIdDelete(@PathVariable("autorhId") Integer autorhId) {
@@ -22,10 +34,45 @@ public class ServiceAuthorsController {
 		return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
 	}
 
-	@RequestMapping(value = "", produces = { "application/json", "text/html" }, method = RequestMethod.POST)
-	public ResponseEntity<ServiceAuthor> serviceAuthorsPost(@RequestBody ServiceAuthor author) {
-		// do some magic!
-		return new ResponseEntity<ServiceAuthor>(HttpStatus.NOT_IMPLEMENTED);
+	/**
+	 * Create Service Author
+	 * @param author
+	 * @param userEPPN
+	 * @return
+	 */
+	@RequestMapping(value = "/service_authors", produces = { "application/json", "text/html" }, method = RequestMethod.POST)
+	public ResponseEntity serviceAuthorsPost(@RequestBody ServiceAuthor author, @RequestHeader(value = "AJP_eppn", required = true) String userEPPN) {
+		int httpStatusCode = HttpStatus.OK.value();
+		Id createdId = null;
+		
+		try {
+			createdId = serviceAuthorDao.createServiceAuthor(author, userEPPN);
+		} catch (DMCServiceException e) {
+			ServiceLogger.logException(logTag, e);
+			return new ResponseEntity<String>(e.getErrorMessage(), e.getHttpStatusCode());
+		}
+
+		return new ResponseEntity<Id>(createdId, HttpStatus.valueOf(httpStatusCode));
 	}
 
+	/**
+	 * Get Service Authors
+	 * @param author
+	 * @param userEPPN
+	 * @return
+	 */
+	@RequestMapping(value = "/services/{id}/service_authors", produces = { "application/json", "text/html" }, method = RequestMethod.GET)
+	public ResponseEntity serviceAuthorsGet(@PathVariable("id") int serviceId,  @RequestHeader(value = "AJP_eppn", required = true) String userEPPN) {
+		int httpStatusCode = HttpStatus.OK.value();
+		ArrayList<ServiceAuthor> authors = null;
+		
+		try {
+			authors = serviceAuthorDao.getServiceAuthors(serviceId, userEPPN);
+		} catch (DMCServiceException e) {
+			ServiceLogger.logException(logTag, e);
+			return new ResponseEntity<String>(e.getErrorMessage(), e.getHttpStatusCode());
+		}
+
+		return new ResponseEntity<ArrayList<ServiceAuthor>>(authors, HttpStatus.valueOf(httpStatusCode));
+	}
 }
