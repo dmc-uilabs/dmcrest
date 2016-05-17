cc998a9a85440fd3aa8de5c2ea9fadd8460c2742
graichen@ge.com <graichen@ge.com>
2016-05-17 09:56:02 -0400

    removed duplicate endpoint, reorder REST type for easier scanning, change from POST to DELETE for new delete implementation

--- src/main/java/org/dmc/services/services/ServiceAuthorsController.java
+++ src/main/java/org/dmc/services/services/ServiceAuthorsController.java
@@ -27,12 +27,12 @@ public class ServiceAuthorsController {
 	private final String logTag = ServiceAuthorsController.class.getName();
 	private ServiceAuthorDao serviceAuthorDao = new ServiceAuthorDao();
 	
-	@RequestMapping(value = "/{autorhId}", produces = { "application/json",
-			"text/html" }, method = RequestMethod.DELETE)
-	public ResponseEntity<Void> serviceAuthorsAutorhIdDelete(@PathVariable("autorhId") Integer autorhId) {
-		// do some magic!
-		return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
-	}
+//	@RequestMapping(value = "/{autorhId}", produces = { "application/json",
+//			"text/html" }, method = RequestMethod.DELETE)
+//	public ResponseEntity<Void> serviceAuthorsAutorhIdDelete(@PathVariable("autorhId") Integer autorhId) {
+//		// do some magic!
+//		return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+//	}
 
 	/**
 	 * Create Service Author
@@ -40,7 +40,7 @@ public class ServiceAuthorsController {
 	 * @param userEPPN
 	 * @return
 	 */
-	@RequestMapping(value = "/service_authors", produces = { "application/json", "text/html" }, method = RequestMethod.POST)
+	@RequestMapping(value = "/service_authors", method = RequestMethod.POST, produces = { "application/json", "text/html" })
 	public ResponseEntity serviceAuthorsPost(@RequestBody ServiceAuthor author, @RequestHeader(value = "AJP_eppn", required = true) String userEPPN) {
 		int httpStatusCode = HttpStatus.OK.value();
 		Id createdId = null;
@@ -61,7 +61,7 @@ public class ServiceAuthorsController {
 	 * @param userEPPN
 	 * @return
 	 */
-	@RequestMapping(value = "/services/{id}/service_authors", produces = { "application/json", "text/html" }, method = RequestMethod.GET)
+	@RequestMapping(value = "/services/{id}/service_authors", method = RequestMethod.GET, produces = { "application/json", "text/html" })
 	public ResponseEntity serviceAuthorsGet(@PathVariable("id") int serviceId,  @RequestHeader(value = "AJP_eppn", required = true) String userEPPN) {
 		int httpStatusCode = HttpStatus.OK.value();
 		ArrayList<ServiceAuthor> authors = null;
@@ -82,7 +82,7 @@ public class ServiceAuthorsController {
 	 * @param userEPPN
 	 * @return
 	 */
-	@RequestMapping(value = "/service_authors", produces = { "application/json", "text/html" }, method = RequestMethod.POST)
+	@RequestMapping(value = "/service_authors", method = RequestMethod.DELETE, produces = { "application/json", "text/html" })
 	public ResponseEntity serviceAuthorsDelete(@PathVariable("id") int authorId, @RequestHeader(value = "AJP_eppn", required = true) String userEPPN) {
 		int httpStatusCode = HttpStatus.OK.value();
 		Id deletedId = null;
