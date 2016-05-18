791e52fedcdd14a4451429109bf4dd155fe9cc3e
Anthony Wamsley <awamsley@nexient.com>
2016-05-18 13:12:33 -0400

    Added Hibernate and spring data jpa configuration

--- src/main/java/org/dmc/services/Application.java
+++ src/main/java/org/dmc/services/Application.java
@@ -1,11 +1,14 @@
 package org.dmc.services;
 
+import javax.sql.DataSource;
+
+import org.postgresql.ds.PGSimpleDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = {SolrAutoConfiguration.class})
 public class Application extends SpringBootServletInitializer {
@@ -17,6 +20,17 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    public DataSource getDataSource() {
+        PGSimpleDataSource ds = new PGSimpleDataSource();
+        ds.setUser(Config.DB_USER);
+        ds.setPassword(Config.DB_PASS);
+        ds.setServerName(Config.DB_HOST);
+        ds.setPortNumber(Integer.parseInt(Config.DB_PORT));
+        ds.setDatabaseName(Config.DB_NAME);
+        return ds;
     }
 
 }
