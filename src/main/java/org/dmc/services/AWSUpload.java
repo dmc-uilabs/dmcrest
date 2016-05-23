159e0c0d84077aff5ad0563a8895c003b634b9a1
Josh Lustig <josh.lustig@uilabs.org>
2016-05-23 09:15:41 -0500

    Security changes to AWS upload, added AWS delete

--- src/main/java/org/dmc/services/AWSUpload.java
+++ src/main/java/org/dmc/services/AWSUpload.java
@@ -13,11 +13,12 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CopyObjectRequest;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+
 
 import org.dmc.services.ServiceLogger;
 import org.dmc.services.services.ServiceController;
 import org.dmc.services.DMCServiceException;
-
 
 /*
  * A Helper class for file uploads and signed URL retrieval from AWS S3 
@@ -26,98 +27,117 @@ import org.dmc.services.DMCServiceException;
 public class AWSUpload {
 	private final String logTag = AWSUpload.class.getName();
 
-	//The Temp Bucket where resource is initially stored by frontend Upload
-	private static String sourceBucket   = "dmc-uploads2";
-	private static String sourceKey      = "dmdii-logo.png";
+	//The Temp Bucket where resource is initially stored by FrontEnd Upload
+	//SOURCE AND DEST BUCKETS SHOULD BE ENV VARIABLES!
+	private static String sourceBucket = "dummy1";//System.getenv("S3SourceBucket");//dmc-uploads2";
+	//private static String sourceKey = "test/cat.jpeg";
 	
 	//The Perm Bucket where resource
-	private static String destBucket     = "dmc-profiletest";
-	private static String destKey        = "dmdii-logo-sanitized.png";
+	private static String destBucket = "dummy2";//System.getenv("S3DestBucket");//"dmc-profiletest";
 
-	private static String accessKey = "AKIAJHHCWWBAHZPREBCQ";
-	private static String secretKey = "SBXDg3GLnoQeOSvDW+g3Zh6Tm0mss0pRFaXbzeDY";
+	private static String accessKey = "dummy3";//System.getenv("S3AccessKey");
+	private static String secretKey = "dummy4";//System.getenv("S3SecretKey");
 
 	
-	
-	//Function should take in sourceKey and destinationKey as parameters
-	public String Upload(String source, String destination, String userEPPN) throws DMCServiceException { 
+	//Source is the path the the resource in the bucket
+	public String Upload(String tempURL, String userEPPN) throws DMCServiceException { 
+		String destPath = "hello"; 
 		
 		ServiceLogger.log(logTag, "User" + userEPPN + "uploading object from " + sourceBucket + " to S3 bucket " + destBucket);
 
 		BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
         AmazonS3 s3client = new AmazonS3Client(awsCreds);
         String preSignedURL = null; 
-
+        
+        
+        //Convert temp URL to sourceKey
+		String sourceKey = tempURL.substring(tempURL.indexOf("com/") + 4, tempURL.length()); 
+		
+        //Convert temp URL to filename 
+        String filename = tempURL.substring(tempURL.lastIndexOf('/') + 1, tempURL.length());
+        
+        
+        //Using a General Hash Code Function for now. Future hash functions should generated unique hashes 
+        String hashCode = Integer.toString(filename.hashCode()%10000); 
+        
+        
+        //Throws error if file invalid 
+        if(filename == null || filename.length() == 0){ 
+        	throw new DMCServiceException(DMCError.Generic, "File from" + userEPPN + "is invalid ");
+        }
+        
+        //Throws error if source path invalid
+        if (destPath == null || destPath.length() == 0){ 
+        	//NEED to add DMC method for AWS exceptions
+        	throw new DMCServiceException(DMCError.Generic, "Source path from" + userEPPN + "is invalid ");
+        }
+        
+        int findDot = filename.lastIndexOf(".");
+  
+        if (findDot == -1){ 
+        	//NEED to add DMC method for AWS exception
+        	throw new DMCServiceException(DMCError.Generic, "User " + userEPPN + "gave Invalid file extension name");
+        }
+        
+        //Rename and Parse
+        String parsedSource = filename.substring(0, findDot) + hashCode + "-sanitized" + filename.substring(findDot, filename.length());     
+        
+        //String Concatenation for location // + destPath + "/" + 
+        String destKey = "Profiles/" + userEPPN + "/"  + parsedSource; 
+       
         try {
         	
             // Copying object, AWS takes care of all implementation of this. 
-            CopyObjectRequest copyObjRequest = new CopyObjectRequest(
-            		sourceBucket, sourceKey, destBucket, destKey);
+            CopyObjectRequest copyObjRequest = new CopyObjectRequest(sourceBucket, sourceKey, destBucket, destKey);
             System.out.println("Copying object.");
             s3client.copyObject(copyObjRequest);
 
         } catch (AmazonServiceException ase) {
         	
-        	//For testing 
-            System.out.println("Caught an AmazonServiceException, " +
-            		"which means your request made it " + 
-            		"to Amazon S3, but was rejected with an error " +
-                    "response for some reason.");
-            
-            
+        	ServiceLogger.log(logTag,"Caught an AmazonServiceException, " + "which means your request made it "
+        	+ "to Amazon S3, but was rejected with an error " + "response for some reason.");
+                  
             //Detailed Error Logging
             ServiceLogger.log(logTag,"Error Message:    " + ase.getMessage());
             ServiceLogger.log(logTag,"HTTP Status Code: " + ase.getStatusCode());
             ServiceLogger.log(logTag,"AWS Error Code:   " + ase.getErrorCode());
             ServiceLogger.log(logTag,"Error Type:       " + ase.getErrorType());
             ServiceLogger.log(logTag,"Request ID:       " + ase.getRequestId());
-			throw new DMCServiceException(DMCError.Generic, "AWS Upload Request from " + userEPPN + " made it, but rejected do to: " + ase.getMessage());
+			throw new DMCServiceException(DMCError.Generic, "AWS Upload Request from " + userEPPN + " made it, rejected do to: " + ase.getMessage());
 
         } catch (AmazonClientException ace) {
         	
-        	//For testing 
-            System.out.println("Caught an AmazonClientException, " +
-            		"which means the client encountered " +
-                    "an internal error while trying to " +
-                    " communicate with S3, " +
-                    "such as not being able to access the network.");
+            ServiceLogger.log(logTag,"Caught an AmazonClientException, which means the client encountered an internal error"
+            + " while trying to communicate with S3, such as not being able to access the network.");
             
             //Detailed Error Logging
             ServiceLogger.log(logTag,"Error Message: " + ace.getMessage());
-			throw new DMCServiceException(DMCError.Generic, "AWS Upload Request from " + userEPPN + " encountered internal error communication with "
-					+ "S3 and rejected do to: " + ace.getMessage());
-        }
-        
-		try {
+			throw new DMCServiceException(DMCError.Generic, "AWS Upload Request from " + userEPPN + " encountered internal error with "
+			+ "S3 and rejected do to: " + ace.getMessage());
+        } try {
+        	
+        	ServiceLogger.log(logTag,"Generating pre-signed URL.");
 			
-			//For testing
-			System.out.println("Generating pre-signed URL.");
-			
-			//Parameters for Request
+			//Parameters for Request, can change expiration time to any amount.
 			java.util.Date expiration = new java.util.Date();
 			long milliSeconds = expiration.getTime();
 			milliSeconds += 1000 * 60 * 60; // Add 1 hour.
 			expiration.setTime(milliSeconds);
 	
-			GeneratePresignedUrlRequest generatePresignedUrlRequest = 
-				    new GeneratePresignedUrlRequest(destBucket, destKey);
+			GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(destBucket, destKey);
 			generatePresignedUrlRequest.setMethod(HttpMethod.GET); 
 			generatePresignedUrlRequest.setExpiration(expiration);
 	
 			URL url = s3client.generatePresignedUrl(generatePresignedUrlRequest); 
 			preSignedURL = url.toString();
-			
-			//For testing
-			System.out.println("Pre-Signed URL = " + preSignedURL);
-						
+									
 		} catch (AmazonServiceException ase) {
 			
 			//For testing
-			System.out.println("Caught an AmazonServiceException, " +
-					"which means your request made it " +
-					"to Amazon S3, but was rejected with an error response " +
-			"for some reason.");
-			   //Detailed Error Logging
+			ServiceLogger.log(logTag,"Caught an AmazonServiceException which means your request made it " 
+			+ "to Amazon S3, but was rejected with an error response for some reason.");
+			
+			//Detailed Error Logging
             ServiceLogger.log(logTag,"Error Message:    " + ase.getMessage());
             ServiceLogger.log(logTag,"HTTP Status Code: " + ase.getStatusCode());
             ServiceLogger.log(logTag,"AWS Error Code:   " + ase.getErrorCode());
@@ -128,20 +148,59 @@ public class AWSUpload {
             
 		} catch (AmazonClientException ace) {
 			
-			//For testing
-			System.out.println("Caught an AmazonClientException, " +
-					"which means the client encountered " +
-					"an internal error while trying to communicate" +
-					" with S3, " +
-			"such as not being able to access the network.");			
-			
+		    ServiceLogger.log(logTag,"Caught an AmazonClientException, which means the client encountered an internal error"
+		    + " while trying to communicate with S3, such as not being able to access the network.");
+		    
 	        //Detailed Error Logging
             ServiceLogger.log(logTag,"Error Message: " + ace.getMessage());
 			throw new DMCServiceException(DMCError.Generic, "AWS Upload Request from " + userEPPN + " encountered internal error communication with "
-					+ "S3 and rejected do to: " + ace.getMessage());
+			+ "S3 and rejected do to: " + ace.getMessage());
 		}
-
-			//If fails
-			return preSignedURL; 
+		return parsedSource; 
 	}
-}
+	
+	public String Remove (String URL, String userEPPN) throws DMCServiceException { 
+
+		//Parse URL to get Path
+		int ResourcePathStart = URL.indexOf("com/") + 4;
+		int ResourcePathEnd = URL.indexOf("?A");
+		String ResourcePath = URL.substring(ResourcePathStart, ResourcePathEnd); 
+		ServiceLogger.log(logTag, "User: " + userEPPN + " deleting " + ResourcePath + " from S3 bucket: " + destBucket);
+
+		BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+        AmazonS3 s3client = new AmazonS3Client(awsCreds);
+        
+        try {
+            // Deleting object, AWS takes care of all implementation of this. 
+            s3client.deleteObject(new DeleteObjectRequest(destBucket, ResourcePath));
+            
+        } catch (AmazonServiceException ase) {
+          	
+        	ServiceLogger.log(logTag,"Caught an AmazonServiceException, " + "which means your request made it "
+        	+ "to Amazon S3, but was rejected with an error " + "response for some reason.");
+                  
+			
+			//Detailed Error Logging
+            ServiceLogger.log(logTag,"Error Message:    " + ase.getMessage());
+            ServiceLogger.log(logTag,"HTTP Status Code: " + ase.getStatusCode());
+            ServiceLogger.log(logTag,"AWS Error Code:   " + ase.getErrorCode());
+            ServiceLogger.log(logTag,"Error Type:       " + ase.getErrorType());
+            ServiceLogger.log(logTag,"Request ID:       " + ase.getRequestId());
+            
+			throw new DMCServiceException(DMCError.Generic, "AWS Delete Request from " + userEPPN + " made it, but rejected do to: " + ase.getMessage());
+        } catch (AmazonClientException ace) {
+        	ServiceLogger.log(logTag,"Caught an AmazonClientException, which means the client encountered an internal error"
+            + " while trying to communicate with S3, such as not being able to access the network.");		
+			
+	        //Detailed Error Logging
+            ServiceLogger.log(logTag,"Error Message: " + ace.getMessage());
+			throw new DMCServiceException(DMCError.Generic, "AWS Delete Request from " + userEPPN + " encountered internal error communication with "
+		    + "S3 and rejected do to: " + ace.getMessage());
+        }  
+        
+        return ResourcePath;
+        
+	}//Remove
+	
+	
+}//End class
