996632ccf1e718b7a1dbf0029273bb619113e093
graichen@ge.com <graichen@ge.com>
2016-05-26 18:17:54 -0400

    Fixed spelling mistake in FollowingMember bean class (with refactoring).

--- src/main/java/org/dmc/services/accounts/FollowingMember.java
+++ src/main/java/org/dmc/services/accounts/FollowingMember.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 
 
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringMVCServerCodegen", date = "2016-04-04T18:05:28.094Z")
-public class FollowingMemeber  {
+public class FollowingMember  {
   
   private String accountId = null;
   private String profileId = null;
@@ -43,7 +43,7 @@ public class FollowingMemeber  {
     if (o == null || getClass() != o.getClass()) {
       return false;
     }
-    FollowingMemeber followingMemeber = (FollowingMemeber) o;
+    FollowingMember followingMemeber = (FollowingMember) o;
     return Objects.equals(accountId, followingMemeber.accountId) &&
         Objects.equals(profileId, followingMemeber.profileId);
   }
