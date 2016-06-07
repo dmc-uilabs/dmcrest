4cd900b092f7775e9676f384e2f151990bf88af1
Ben Beckmann <beckmann@ge.com>
2016-06-06 21:10:02 -0400

    Updated content type in multiple task enpoint and their tests

--- src/main/java/org/dmc/services/tasks/TaskController.java
+++ src/main/java/org/dmc/services/tasks/TaskController.java
@@ -21,22 +21,18 @@ public class TaskController {
 	private final String logTag = "TASK_CONTROLLER";
 	private TaskDao task = new TaskDao();
 	
-	@RequestMapping(value = "/tasks/{taskID}", method = RequestMethod.GET)
+	@RequestMapping(value = "/tasks/{taskID}", method = RequestMethod.GET, consumes = "application/json", produces = "application/json")
 	public Task getTask(@PathVariable("taskID") String taskID, @RequestHeader(value="AJP_eppn", defaultValue="testUser") String userEPPN) {
 		ServiceLogger.log(logTag, "UserName: " + userEPPN);
 		return task.getTask(taskID);
 	}
 	
 	 
-	  @RequestMapping(value = "/tasks/{taskID}", 
-	    produces = { "application/json", "text/html" }, 
-	    method = RequestMethod.DELETE)
-	  public ResponseEntity<Void> tasksTaskIDDelete(
-	@PathVariable("taskID") String taskID
-	){
+	@RequestMapping(value = "/tasks/{taskID}", method = RequestMethod.DELETE, consumes = "application/json", produces = "application/json")
+	public ResponseEntity<Void> tasksTaskIDDelete(@PathVariable("taskID") String taskID){
 	      // do some magic!
 	      return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
-	  }
+	}
 
 	
     @RequestMapping(value = "/tasks/create", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
@@ -51,7 +47,7 @@ public class TaskController {
 		return task.getTaskList();
 	}
 	
-	@RequestMapping(value = "/projects/{projectID}/tasks", method = RequestMethod.GET)
+	@RequestMapping(value = "/projects/{projectID}/tasks", method = RequestMethod.GET, consumes = "application/json", produces = "application/json")
 	public ArrayList<Task> getTaskList(@PathVariable("projectID") int projectId, @RequestHeader(value="AJP_eppn", defaultValue="testUser") String userEPPN) {
 		ServiceLogger.log(logTag, "UserName: " + userEPPN);
 		return task.getTaskList(projectId);
--- src/test/java/org/dmc/services/TaskIT.java
+++ src/test/java/org/dmc/services/TaskIT.java
@@ -30,23 +30,30 @@ public class TaskIT extends BaseIT {
 			.body(matchesJsonSchemaInClasspath("Schemas/idSchema.json"))
 		.extract().path("id");
 	  
-	  String newGetRequest = "/tasks/" + id.toString();
+		String newGetRequest = "/tasks/" + id.toString();
 	
-	  // let's query the newly created task and make sure we get it
-	  expect().
-	    statusCode(200).
+		// let's query the newly created task and make sure we get it
+		given().
+			header("Content-type", "application/json").
+		expect().
+			statusCode(200).
 	    when().
-	    get(newGetRequest).then().
-	    log().all().
-        body(matchesJsonSchemaInClasspath("Schemas/taskSchema.json"));
+			get(newGetRequest).
+		then().
+			log().all().body(matchesJsonSchemaInClasspath("Schemas/taskSchema.json"));
 	}
 	
 	// WARNING: this test is ok as long as our test db has task with id = 1
 	@Test
 	public void testTask1(){
-		expect().statusCode(200).when().get("/tasks/1").then().
-		log().all().
-        body(matchesJsonSchemaInClasspath("Schemas/taskSchema.json"));
+		given().
+			header("Content-type", "application/json").
+		expect().
+			statusCode(200).
+		when().
+			get("/tasks/1").
+		then().
+			log().all().body(matchesJsonSchemaInClasspath("Schemas/taskSchema.json"));
 	}
 	
 	@Test
@@ -96,6 +103,7 @@ public class TaskIT extends BaseIT {
 	@Test
 	public void testDelete_FollowDiscussions(){
 		given().
+		header("Content-type", "application/json").
 		header("AJP_eppn", userEPPN).
 		expect().
 		statusCode(HttpStatus.NOT_IMPLEMENTED.value()).
