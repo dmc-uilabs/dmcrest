14753c08ccdd22b4dd84af85eca2b33b1cbef2e4
Bowden Wise <wisegb@ge.com>
2016-06-13 12:21:38 -0400

    Added extend BaseIt

--- src/test/java/org/dmc/services/CompanyReviewIT.java
+++ src/test/java/org/dmc/services/CompanyReviewIT.java
@@ -22,7 +22,7 @@ import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.junit.Assert.assertTrue;
 
-public class CompanyReviewIT {
+public class CompanyReviewIT extends BaseIT {
 
     private static final String logTag = CompanyReviewIT.class.getName();
 
