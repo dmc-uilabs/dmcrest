2323c905c869f18cf19cf5c9b8bb643236a5594d
Max Rowland <mrowland@nexient.com>
2016-06-17 13:55:53 -0400

    Fixing a missed merge

--- src/main/java/org/dmc/services/dmdiimember/DMDIIMemberService.java
+++ src/main/java/org/dmc/services/dmdiimember/DMDIIMemberService.java
@@ -10,10 +10,12 @@ import javax.inject.Inject;
 
 import org.dmc.services.data.entities.DMDIIMember;
 import org.dmc.services.data.entities.Organization;
+import org.dmc.services.data.entities.QDMDIIMember;
 import org.dmc.services.data.mappers.Mapper;
 import org.dmc.services.data.mappers.MapperFactory;
 import org.dmc.services.data.models.DMDIIMemberModel;
 import org.dmc.services.data.models.OrganizationModel;
+import org.dmc.services.exceptions.InvalidFilterParameterException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
@@ -37,7 +39,7 @@ public class DMDIIMemberService {
 		Mapper<DMDIIMember, DMDIIMemberModel> mapper = mapperFactory.mapperFor(DMDIIMember.class, DMDIIMemberModel.class);
 		return mapper.mapToModel(dmdiiMemberDao.findOne(id));
 	}
-	
+
 	public List<DMDIIMemberModel> findByName(String name, Integer pageNumber, Integer pageSize) {
 		Mapper<DMDIIMember, DMDIIMemberModel> mapper = mapperFactory.mapperFor(DMDIIMember.class, DMDIIMemberModel.class);
 		return mapper.mapToModel(dmdiiMemberDao.findByOrganizationNameLikeIgnoreCase(new PageRequest(pageNumber, pageSize), "%"+name+"%").getContent());
@@ -57,58 +59,58 @@ public class DMDIIMemberService {
 
 		return memberMapper.mapToModel(memberEntity);
 	}
-	
+
 	public List<DMDIIMemberModel> filter(Map filterParams, Integer pageNumber, Integer pageSize) throws InvalidFilterParameterException {
 		Mapper<DMDIIMember, DMDIIMemberModel> mapper = mapperFactory.mapperFor(DMDIIMember.class, DMDIIMemberModel.class);
 		Predicate where = ExpressionUtils.allOf(getFilterExpressions(filterParams));
 		return mapper.mapToModel(dmdiiMemberDao.findAll(where, new PageRequest(pageNumber, pageSize)).getContent());
 	}
-	
+
 	private Collection<Predicate> getFilterExpressions(Map<String, String> filterParams) throws InvalidFilterParameterException {
 		Collection<Predicate> expressions = new ArrayList<Predicate>();
-		
+
 		expressions.add(categoryIdFilter(filterParams.get("categoryId")));
 		expressions.add(tierFilter(filterParams.get("tier")));
 		expressions.add(hasActiveProjectsFilter(filterParams.get("hasActiveProjects")));
-		
+
 		return expressions;
 	}
-	
+
 	private Predicate categoryIdFilter(String categoryId) throws InvalidFilterParameterException {
 		if (categoryId == null) {
 			return null;
 		}
-		
+
 		Integer categoryIdInt = null;
 		try {
 			categoryIdInt = Integer.parseInt(categoryId);
 		} catch (NumberFormatException e) {
 			throw new InvalidFilterParameterException("categoryId", Integer.class);
 		}
-		
+
 		return QDMDIIMember.dMDIIMember.dmdiiType().dmdiiTypeCategory().id.eq(categoryIdInt);
 	}
-	
+
 	private Predicate tierFilter(String tier) throws InvalidFilterParameterException {
 		if (tier == null) {
 			return null;
 		}
-		
+
 		Integer tierInt = null;
 		try {
 			tierInt = Integer.parseInt(tier);
 		} catch (NumberFormatException e) {
 			throw new InvalidFilterParameterException("tier", Integer.class);
 		}
-		
+
 		return QDMDIIMember.dMDIIMember.dmdiiType().tier.eq(tierInt);
 	}
-	
+
 	private Predicate hasActiveProjectsFilter(String hasActiveProjects) {
 		if (hasActiveProjects == null) {
 			return null;
 		}
-		
+
 		Date today = new Date();
 		if (Boolean.valueOf(hasActiveProjects)) {
 			return QDMDIIMember.dMDIIMember.projects.any().awardedDate.before(today).and(
