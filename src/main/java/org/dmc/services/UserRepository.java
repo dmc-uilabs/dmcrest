124a143c9fe409361994551fff5231f14294b4ef
Webbski <jwebb@nexient.com>
2016-06-30 13:53:39 -0400

    Changes to DMDIIDocument files to reflect new data structure and added endpoints

---
+++ src/main/java/org/dmc/services/UserRepository.java
@@ -0,0 +1,8 @@
+package org.dmc.services;
+
+import org.dmc.services.data.entities.User;
+import org.dmc.services.data.repositories.BaseRepository;
+
+public interface UserRepository extends BaseRepository<User, Integer> {
+
+}
