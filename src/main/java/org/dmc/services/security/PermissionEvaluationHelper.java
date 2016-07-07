2b6831811314a192032fb581580c718cd470883f
Anthony Wamsley <awamsley@nexient.com>
2016-07-07 14:27:01 -0400

    Extracted permission evaluation into helper class

---
+++ src/main/java/org/dmc/services/security/PermissionEvaluationHelper.java
@@ -0,0 +1,28 @@
+package org.dmc.services.security;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class PermissionEvaluationHelper {
+	
+	public static boolean userHasRole(String requiredRole, Integer tenantId) {
+		UserPrincipal user = (UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		List<String> userRoles = new ArrayList<String>();
+		if (user.getIsSuperAdmin()) {
+			return true;
+		}
+		String assignedRole = user.getRole(tenantId);
+		switch (assignedRole) {
+		case SecurityRoles.ADMIN:
+			userRoles.add(SecurityRoles.ADMIN);
+		case SecurityRoles.VIP:
+			userRoles.add(SecurityRoles.VIP);
+		case SecurityRoles.MEMBER:
+			userRoles.add(SecurityRoles.MEMBER);
+		}
+		return userRoles.contains(requiredRole);
+	}
+
+}
