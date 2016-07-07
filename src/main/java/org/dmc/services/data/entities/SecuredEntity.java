b36ad6a8ca55025a0b3cd00cc96cfc906ef2a602
Anthony Wamsley <awamsley@nexient.com>
2016-07-07 14:21:54 -0400

    Added comments to explain security classes

--- src/main/java/org/dmc/services/data/entities/SecuredEntity.java
+++ src/main/java/org/dmc/services/data/entities/SecuredEntity.java
@@ -1,7 +1,14 @@
 package org.dmc.services.data.entities;
 
+/**
+ * @author awamsley
+ * This interface must be implemented by entities that are using the @RequiredPermission annotation
+ */
 public interface SecuredEntity {
 	
+	/**
+	 * This must return the id for the organization that owns the entity instance
+	 */
 	Integer getTenantId();
 
 }
