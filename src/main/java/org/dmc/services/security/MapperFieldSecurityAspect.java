31c1b937edee059ae2375a403f8e54d2343ac5a0
Anthony Wamsley <awamsley@nexient.com>
2016-07-07 14:27:01 -0400

    Extracted permission evaluation into helper class

--- src/main/java/org/dmc/services/security/MapperFieldSecurityAspect.java
+++ src/main/java/org/dmc/services/security/MapperFieldSecurityAspect.java
@@ -1,13 +1,10 @@
 package org.dmc.services.security;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.dmc.services.data.entities.SecuredEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -20,28 +17,11 @@ public class MapperFieldSecurityAspect {
 			if (field.isAnnotationPresent(RequiredPermission.class)) {
 				field.setAccessible(true);
 				String requiredRole = field.getAnnotation(RequiredPermission.class).value();
-				if (!userHasRole(requiredRole, entity.getTenantId())) {
+				if (!PermissionEvaluationHelper.userHasRole(requiredRole, entity.getTenantId())) {
 					field.set(entity, null);
 				}
 			}
 		}
 	}
 	
-	private boolean userHasRole(String requiredRole, Integer tenantId) {
-		UserPrincipal user = (UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		List<String> userRoles = new ArrayList<String>();
-		if (user.getIsSuperAdmin()) {
-			return true;
-		}
-		String assignedRole = user.getRole(tenantId);
-		switch (assignedRole) {
-		case SecurityRoles.ADMIN:
-			userRoles.add(SecurityRoles.ADMIN);
-		case SecurityRoles.VIP:
-			userRoles.add(SecurityRoles.VIP);
-		case SecurityRoles.MEMBER:
-			userRoles.add(SecurityRoles.MEMBER);
-		}
-		return userRoles.contains(requiredRole);
-	}
 }
