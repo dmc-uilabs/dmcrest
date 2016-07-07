59878568220ced6f787b45d1812d9ad76e3f150f
Jerome <jerome.francis@ge.com>
2016-07-07 10:39:37 -0400

    update to test to check if URL has DOMEApiServicesV7

--- src/test/java/org/dmc/services/accounts/AccountServerIT.java
+++ src/test/java/org/dmc/services/accounts/AccountServerIT.java
@@ -28,242 +28,244 @@ import org.dmc.services.ServiceLogger;
 @SuppressWarnings("unused")
 public class AccountServerIT extends BaseIT {
 	private final String logTag = AccountsIT.class.getName();
-	
+
 	private String newKnownUser;
 	private String uniqueID;
 	private String JSON = org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 	private final String validURL = System.getenv("RunningDOMEip");
-	
+
 	//These can be used when the Apache http client is used to test connections instead of the Sun/Oracle HTTP client
 	//private final String urlOff = System.getenv("ServerIsOff");
 	//private final String tomcatIsOff = System.getenv("TestDOMEnotRunning");
-	
-	
+
+
 	private int user_id_lookedup;
-	
+
 	@Before
 	public void setupData() {
-	    newKnownUser = TestUserUtil.createNewUser();
-	    uniqueID = TestUserUtil.uniqueID();
-		
-	    try{
-		user_id_lookedup = UserDao.getUserID(newKnownUser);
-	    } catch (SQLException e) {
-		assertTrue("Error looking up new user", false);
-	    }
+		newKnownUser = TestUserUtil.createNewUser();
+		uniqueID = TestUserUtil.uniqueID();
+
+		try{
+			user_id_lookedup = UserDao.getUserID(newKnownUser);
+		} catch (SQLException e) {
+			assertTrue("Error looking up new user", false);
+		}
 	}
-	
-	
+
+
 	/**
 	 * Tests for
 	 * <code> post /account-servers </code>
 	 **/
-	
+
 	@Test
 	public void testAccountPost_NewServer_valid_server() {
 		UserAccountServer userAccountServer = new UserAccountServer();
-		
+
 		// setup userAccountServer
 		userAccountServer.setAccountId(Integer.toString(user_id_lookedup));
 		userAccountServer.setName("ServerName" + uniqueID);
 		userAccountServer.setIp(validURL);
 		userAccountServer.setStatus("online");
 		// ToDo: add server status to database
-//		userAccountServer.setStatus("ServerStatus" + uniqueID);
-		
+		//		userAccountServer.setStatus("ServerStatus" + uniqueID);
+
 		UserAccountServer returnedUserAccountServer = createNewServer(userAccountServer);
-				
+
 		// check that the orginal UserAccountServer object does not have an id
 		assertTrue("Orginal server id is not null",
-				   userAccountServer.getId() == null);
-		
+				userAccountServer.getId() == null);
+
 		// set if of orginal to id of returned
 		userAccountServer.setId(returnedUserAccountServer.getId());
-		userAccountServer.setIp(validURL + "/DOMEApiServicesV7/");
-		
+
+		if (!returnedUserAccountServer.getIp().contains("DOMEApiServicesV7/"))
+			userAccountServer.setIp(validURL + "/DOMEApiServicesV7/");
+
 		// check returned and orginal UserAccountServer object is equal
 		assertTrue("Orginal and returned UserAccountServer objects are not equal",
-				   returnedUserAccountServer.equals(userAccountServer));
+				returnedUserAccountServer.equals(userAccountServer));
 	}
-	
+
 	@Test
 	public void testAccountPost_NewServer_invalid_server() {
 		UserAccountServer userAccountServer = new UserAccountServer();
-		
+
 		// setup userAccountServer
 		userAccountServer.setAccountId(Integer.toString(user_id_lookedup));
 		userAccountServer.setName("ServerName" + uniqueID);
 		userAccountServer.setIp("ServerURL" + uniqueID);
 		// ToDo: add server status to database
-//		userAccountServer.setStatus("ServerStatus" + uniqueID);
+		//		userAccountServer.setStatus("ServerStatus" + uniqueID);
 		ObjectMapper mapper = new ObjectMapper();
 		String userAccountServerString = null;
-		
+
 		try {
 			userAccountServerString = mapper.writeValueAsString(userAccountServer);
 		} catch (JsonProcessingException e) {
-			
+
 			ServiceLogger.log(logTag, e.getMessage());
 		}
-		
-	
-				given().
-					header("Content-type", JSON).
-					header("AJP_eppn", newKnownUser).
-				body(userAccountServerString).
-					expect().
-				statusCode(HttpStatus.UNPROCESSABLE_ENTITY.value()). 
-				post("/account_servers");
+
+
+		given().
+		header("Content-type", JSON).
+		header("AJP_eppn", newKnownUser).
+		body(userAccountServerString).
+		expect().
+		statusCode(HttpStatus.UNPROCESSABLE_ENTITY.value()). 
+		post("/account_servers");
 	}
-	
+
 	/**
 	 * Tests for
 	 * <code> get /account-servers/{serverID} </code>
 	 **/
 	@Ignore //this test requires some extra functionality allowing a tester to enter invalid data into the database
 	//due to the existing POST requests performing checks that disallow this, this test must currently be ignored 
-	
+
 	@Test
 	public void testAccountGet_ServerID_invalid_Server() {
 		UserAccountServer returnedUserAccountServer = createNewServerNoCheck();
-	
+
 		given().
-			header("Content-type", JSON).
-			header("AJP_eppn", newKnownUser).
+		header("Content-type", JSON).
+		header("AJP_eppn", newKnownUser).
 		expect().
-			statusCode(HttpStatus.UNPROCESSABLE_ENTITY.value()). 
+		statusCode(HttpStatus.UNPROCESSABLE_ENTITY.value()). 
 		when().
-			get("/account_servers/" + returnedUserAccountServer.getId());
+		get("/account_servers/" + returnedUserAccountServer.getId());
 	}
-	
+
 	@Test
 	public void testAccountGet_ServerID_valid_server() {
 		UserAccountServer userAccountServer = new UserAccountServer();
-		
+
 		// setup userAccountServer
 		userAccountServer.setAccountId(Integer.toString(user_id_lookedup));
 		userAccountServer.setName("ServerName" + uniqueID);
 		userAccountServer.setIp(validURL); 
 		userAccountServer.setStatus("online");
-		
+
 		UserAccountServer returnedUserAccountServer = createNewServer(userAccountServer);
-		
+
 		UserAccountServer getReturnedUserAccountServer =
-		given().
-			header("Content-type", JSON).
-			header("AJP_eppn", newKnownUser).
-		expect().
-			statusCode(HttpStatus.OK.value()).  
-		when().
-			get("/account_servers/" + returnedUserAccountServer.getId()).
-			as(UserAccountServer.class);
-				
+				given().
+				header("Content-type", JSON).
+				header("AJP_eppn", newKnownUser).
+				expect().
+				statusCode(HttpStatus.OK.value()).  
+				when().
+				get("/account_servers/" + returnedUserAccountServer.getId()).
+				as(UserAccountServer.class);
+
 		// check returned and orginal UserAccountServer object is equal
 		ServiceLogger.log(logTag, "status for original server: " + returnedUserAccountServer.getStatus()
 		+ "\tstatus for getObject: " + getReturnedUserAccountServer.getStatus());
 		assertTrue("Orginal and returned UserAccountServer objects are not equal",
-		       	   returnedUserAccountServer.equals(getReturnedUserAccountServer));
+				returnedUserAccountServer.equals(getReturnedUserAccountServer));
 	}
-	
-	
+
+
 	/**
 	 * Tests for
 	 * <code> DELETE /account-servers/{serverID} </code>
 	 **/
-	
+
 	@Test
 	public void testAccountDelete_ServerID() {
 		UserAccountServer returnedUserAccountServer = createNewServer();
-		
+
 		// delete server record
 		given().
-			header("Content-type", JSON).
-			header("AJP_eppn", newKnownUser).
+		header("Content-type", JSON).
+		header("AJP_eppn", newKnownUser).
 		expect().
-			statusCode(HttpStatus.OK.value()).
+		statusCode(HttpStatus.OK.value()).
 		when().
-			delete("/account_servers/" + returnedUserAccountServer.getId());
-		
+		delete("/account_servers/" + returnedUserAccountServer.getId());
+
 		// attempt to retrieve deleted record; fails
 		given().
-			header("Content-type", JSON).
-			header("AJP_eppn", newKnownUser).
+		header("Content-type", JSON).
+		header("AJP_eppn", newKnownUser).
 		expect().
-			statusCode(HttpStatus.NO_CONTENT.value()).
+		statusCode(HttpStatus.NO_CONTENT.value()).
 		when().
-			get("/account_servers/" + returnedUserAccountServer.getId());
-		
+		get("/account_servers/" + returnedUserAccountServer.getId());
+
 	}
-	
+
 	/**
 	 * Tests for
 	 * <code> PATCH /account-servers/{serverID} </code>
 	 **/
-	
+
 	@Test
 	public void testAccountPatch_ServerID_invalid_server() {
 		UserAccountServer returnedUserAccountServer = createNewServer();
 		ObjectMapper mapper = new ObjectMapper();
 		String userAccountServerString = null;
-		
+
 		returnedUserAccountServer.setName("patchedName");
 		returnedUserAccountServer.setIp("patchedIP");
-		
+
 		try {
 			userAccountServerString = mapper.writeValueAsString(returnedUserAccountServer);
 		} catch (JsonProcessingException e) {
 			ServiceLogger.log(logTag, e.getMessage());
 		}
-		
+
 		given().
-			header("Content-type", JSON).
-			header("AJP_eppn", newKnownUser).
-			body(userAccountServerString).
+		header("Content-type", JSON).
+		header("AJP_eppn", newKnownUser).
+		body(userAccountServerString).
 		expect().
-			statusCode(HttpStatus.UNPROCESSABLE_ENTITY.value()).
+		statusCode(HttpStatus.UNPROCESSABLE_ENTITY.value()).
 		when().
-			patch("/account_servers/" + returnedUserAccountServer.getId());
+		patch("/account_servers/" + returnedUserAccountServer.getId());
 	}
-	
+
 	@Test
 	public void testAccountPatch_ServerID_valid_server() {
 		UserAccountServer returnedUserAccountServer = createNewServer();
 		ObjectMapper mapper = new ObjectMapper();
 		String userAccountServerString = null;
-		
+
 		returnedUserAccountServer.setName("updatedWorkingName");
 		returnedUserAccountServer.setIp(validURL);
 		returnedUserAccountServer.setStatus("online");
-		
+
 		try {
 			userAccountServerString = mapper.writeValueAsString(returnedUserAccountServer);
 		} catch (JsonProcessingException e) {
 			ServiceLogger.log(logTag, e.getMessage());
 		}
-		
+
 		UserAccountServer patchedUserAccountServer =
-		given().
-			header("Content-type", JSON).
-			header("AJP_eppn", newKnownUser).
-			body(userAccountServerString).
-		expect().
-			statusCode(HttpStatus.OK.value()).
-		when().
-			patch("/account_servers/" + returnedUserAccountServer.getId()).
-			as(UserAccountServer.class);
-		
-		returnedUserAccountServer.setIp(validURL + "/DOMEApiServicesV7/");
+				given().
+				header("Content-type", JSON).
+				header("AJP_eppn", newKnownUser).
+				body(userAccountServerString).
+				expect().
+				statusCode(HttpStatus.OK.value()).
+				when().
+				patch("/account_servers/" + returnedUserAccountServer.getId()).
+				as(UserAccountServer.class);
+		if (!returnedUserAccountServer.getIp().contains("DOMEApiServicesV7/"))
+			returnedUserAccountServer.setIp(validURL + "/DOMEApiServicesV7/");
 		// check returned and orginal UserAccountServer object is equal
 		assertTrue("Orginal modified UserAccountServer object is not equal to patched",
-       			   returnedUserAccountServer.equals(patchedUserAccountServer));
+				returnedUserAccountServer.equals(patchedUserAccountServer));
 	}
-	
-	
+
+
 	private UserAccountServer createNewServer(UserAccountServer userAccountServer) {
 		ObjectMapper mapper = new ObjectMapper();
 		String userAccountServerString = null;
 		String jsonString = "unset", error = "unset";
-		
+
 		try {
 			userAccountServerString = mapper.writeValueAsString(userAccountServer);
 			jsonString = userAccountServer.toString();
@@ -271,61 +273,61 @@ public class AccountServerIT extends BaseIT {
 			error = e.getMessage();
 			ServiceLogger.log(logTag, e.getMessage());
 		}
-		
-		
-		
+
+
+
 		UserAccountServer returnedUserAccountServer =
-		given().
-			header("Content-type", JSON).
-			header("AJP_eppn", newKnownUser).
-		body(userAccountServerString).
-			expect().
-		statusCode(HttpStatus.CREATED.value()). 
-			when().
-		post("/account_servers"). //IMPROVEMENT allow forced user input for testing purposes
-			as(UserAccountServer.class);
-		
+				given().
+				header("Content-type", JSON).
+				header("AJP_eppn", newKnownUser).
+				body(userAccountServerString).
+				expect().
+				statusCode(HttpStatus.CREATED.value()). 
+				when().
+				post("/account_servers"). //IMPROVEMENT allow forced user input for testing purposes
+				as(UserAccountServer.class);
+
 		return returnedUserAccountServer;
 	}
-	
+
 	private UserAccountServer createNewServer() {
 		UserAccountServer userAccountServer = new UserAccountServer();
-		
+
 		// setup userAccountServer
 		userAccountServer.setAccountId(Integer.toString(user_id_lookedup));
 		userAccountServer.setName("ServerName" + uniqueID);
 		userAccountServer.setIp(validURL);
 		return createNewServer(userAccountServer);
 	}
-	
+
 	private UserAccountServer createNewServerNoCheck(){
 		UserAccountServer userAccountServer = new UserAccountServer();
-		
+
 		// setup userAccountServer
 		userAccountServer.setAccountId(Integer.toString(user_id_lookedup));
 		userAccountServer.setName("ServerName" + uniqueID);
 		userAccountServer.setIp("invalidIP");
-		
+
 		ObjectMapper mapper = new ObjectMapper();
 		String userAccountServerString = null;
-		
+
 		try {
 			userAccountServerString = mapper.writeValueAsString(userAccountServer);
 		} catch (JsonProcessingException e) {
 			ServiceLogger.log(logTag, e.getMessage());
 		}
-		
-	
+
+
 		given().
-			header("Content-type", JSON).
-			header("AJP_eppn", newKnownUser).
+		header("Content-type", JSON).
+		header("AJP_eppn", newKnownUser).
 		body(userAccountServerString).
-			expect().
+		expect().
 		statusCode(HttpStatus.UNPROCESSABLE_ENTITY.value()).  
-			when().
+		when().
 		post("/account_servers"); //IMPROVEMENT allow forced user input for testing purposes
-		
-		
+
+
 		return userAccountServer;
 	}
 }
