7511cc2c4fed17b59f11808f5284d64040a40767
Max Rowland <mrowland@nexient.com>
2016-07-19 16:17:45 -0400

    Adding new entities for the private/public/portal contact info, added relations with user entity, made a /user/{id} endpoint to get user by id.

---
+++ src/main/java/org/dmc/services/data/entities/UserPublicContactInfo.java
@@ -0,0 +1,57 @@
+package org.dmc.services.data.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_public_contact_info")
+public class UserPublicContactInfo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+
+	private String email;
+
+	private String phone;
+
+	private String location;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+}
