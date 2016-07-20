f0980b10c521b2a14f1486124aafdc01bada593a
Max Rowland <mrowland@nexient.com>
2016-07-20 17:08:35 -0400

    Adding hash and equals functions to each new entity, adding to hash and equals function of user.java as well.

--- src/main/java/org/dmc/services/data/entities/UserContactInfo.java
+++ src/main/java/org/dmc/services/data/entities/UserContactInfo.java
@@ -63,4 +63,47 @@ public class UserContactInfo extends BaseEntity {
 		this.userMemberPortalContactInfo = userMemberPortalContactInfo;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((userPrivateContactInfo == null) ? 0 : userPrivateContactInfo.hashCode());
+		result = prime * result + ((userPublicContactInfo == null) ? 0 : userPublicContactInfo.hashCode());
+		result = prime * result + ((userMemberPortalContactInfo == null) ? 0 : userMemberPortalContactInfo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserContactInfo other = (UserContactInfo) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (userPrivateContactInfo == null) {
+			if (other.userPrivateContactInfo != null)
+				return false;
+		} else if (!userPrivateContactInfo.equals(other.userPrivateContactInfo))
+			return false;
+		if (userPublicContactInfo == null) {
+			if (other.userPublicContactInfo != null)
+				return false;
+		} else if (!userPublicContactInfo.equals(other.userPublicContactInfo))
+			return false;
+		if (userMemberPortalContactInfo == null) {
+			if (other.userMemberPortalContactInfo != null)
+				return false;
+		} else if (!id.equals(other.userMemberPortalContactInfo))
+			return false;
+		return true;
+	}
+
 }
