f7ad531b82bbb463482468d881a9d70709a223cb
Max Rowland <mrowland@nexient.com>
2016-07-20 17:08:35 -0400

    Adding hash and equals functions to each new entity, adding to hash and equals function of user.java as well.

--- src/main/java/org/dmc/services/data/entities/UserPublicContactInfo.java
+++ src/main/java/org/dmc/services/data/entities/UserPublicContactInfo.java
@@ -54,4 +54,47 @@ public class UserPublicContactInfo extends BaseEntity {
 		this.location = location;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+		result = prime * result + ((location == null) ? 0 : location.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserPublicContactInfo other = (UserPublicContactInfo) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (phone == null) {
+			if (other.phone != null)
+				return false;
+		} else if (!phone.equals(other.phone))
+			return false;
+		if (location == null) {
+			if (other.location != null)
+				return false;
+		} else if (!location.equals(other.location))
+			return false;
+		return true;
+	}
+
 }
