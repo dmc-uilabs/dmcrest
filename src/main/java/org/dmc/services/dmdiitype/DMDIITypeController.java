9c2394652c56d476cd5efcc0dbe44bb936db661f
Anthony Wamsley <awamsley@nexient.com>
2016-07-25 10:15:07 -0400

    Secure member portal endpoints

--- src/main/java/org/dmc/services/dmdiitype/DMDIITypeController.java
+++ src/main/java/org/dmc/services/dmdiitype/DMDIITypeController.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.dmc.services.security.SecurityRoles;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@PreAuthorize(SecurityRoles.REQUIRED_ROLE_MEMBER)
 public class DMDIITypeController {
 
 	@Inject
