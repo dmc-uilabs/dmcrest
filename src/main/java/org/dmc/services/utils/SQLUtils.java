fc350f2e96a4afacdd3617acf8b221b8453bfbae
graichen@ge.com <graichen@ge.com>
2016-08-01 18:56:16 -0400

    expanded tests for creating order by and limit clauses reformatted (spaces for tabs) send list of allowed fields when creating order by clause.

--- src/main/java/org/dmc/services/utils/SQLUtils.java
+++ src/main/java/org/dmc/services/utils/SQLUtils.java
@@ -1,5 +1,7 @@
 package org.dmc.services.utils;
 
+import java.util.List;
+
 import org.dmc.services.DMCError;
 import org.dmc.services.DMCServiceException;
 
@@ -19,20 +21,48 @@ public class SQLUtils {
      * @param sort
      * @return null or the ORDER BY clause
      */
-    public static String buildOrderByClause (String order,  String sort)  {
+    public static String buildOrderByClause (String order,  String sort, List<String> validSortFields)  {
 
         String orderByClause = null;
+        boolean lastFieldHasOrder = false;
         if (sort != null && sort.trim().length() > 0) {
-
-            orderByClause = "ORDER " + sort.trim();
+            orderByClause = "ORDER ";
+            final String[] tokens = sort.split(",");
+            for (String token : tokens) {
+                final String[] parts = token.trim().split(" +");
+                if (parts.length > 2) {
+                    throw new DMCServiceException(DMCError.BadURL, "invalid sort field: " + token.trim() + " parts.length=" + parts.length);
+                }
+                if (!validSortFields.contains(parts[0].trim())) {
+                    throw new DMCServiceException(DMCError.BadURL, "invalid sort field: " + token.trim());
+                }
+                orderByClause += parts[0];
+                if (parts.length > 1) {
+                    if (parts[1].equals(SORT_ASCENDING) || parts[1].equals(SORT_DESCENDING)) {
+                        orderByClause += " " + parts[1];
+                        lastFieldHasOrder = true;
+                    } else {
+                        throw new DMCServiceException(DMCError.BadURL, "invalid sort option " + parts[1] + " on field " + parts[0]);
+                    }
+                } else {
+                    lastFieldHasOrder = false;
+                }
+                orderByClause += ", ";
+            }
         }
 
+        if (orderByClause != null) {
+            orderByClause = orderByClause.substring(0, orderByClause.length()-2);   // remove last ", "
+        }
         // order only added if ORDER BY
         if (orderByClause != null && order != null && order.trim().length() >0) {
+            if (lastFieldHasOrder) {
+                throw new DMCServiceException(DMCError.BadURL, "specified order, but last field already had order");
+            }
             if (order.equals(SORT_ASCENDING) || order.equals(SORT_DESCENDING)) {
                 orderByClause += " " + order;
             } else {
-                throw new DMCServiceException(DMCError.BadURL, "invalid sort option");
+                throw new DMCServiceException(DMCError.BadURL, "invalid sort option: " + order);
             }
         }
 
--- src/test/java/org/dmc/services/utils/SQLUtilsTest.java
+++ src/test/java/org/dmc/services/utils/SQLUtilsTest.java
@@ -2,67 +2,106 @@ package org.dmc.services.utils;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
+
 import org.dmc.services.DMCServiceException;
 
 public class SQLUtilsTest {
+    private final ArrayList<String> validSortFields = new ArrayList<String>();
+    
+    @Before
+    public void setup() {
+        validSortFields.add("xxx");
+        validSortFields.add("abc");
+    }
 
     @Test
     public void setsStringToNullIfNoOrderOrSort() {
-        String orderByClause = SQLUtils.buildOrderByClause(null, null);
+        final String orderByClause = SQLUtils.buildOrderByClause(null, null, validSortFields);
         assertNull(orderByClause);
     }
 
     @Test
     public void setsStringToNullIfEmptyOrderAndSort() {
-        String orderByClause = SQLUtils.buildOrderByClause("", "");
+        final String orderByClause = SQLUtils.buildOrderByClause("", "", validSortFields);
         assertNull(orderByClause);
     }
 
     @Test
     public void setsOrderToStringWithoutSortOption() {
-        String orderByClause = SQLUtils.buildOrderByClause(null, "abc");
+        final String orderByClause = SQLUtils.buildOrderByClause(null, "abc", validSortFields);
         assertEquals("ORDER abc", orderByClause);
     }
 
     @Test
     public void setsOrderToTrimmedStringWithoutSortOption() {
-        String orderByClause = SQLUtils.buildOrderByClause(null, " abc ");
+        final String orderByClause = SQLUtils.buildOrderByClause(null, " abc ", validSortFields);
         assertEquals("ORDER abc", orderByClause);
     }
 
     @Test
     public void setsStringToNullIfSortIsNull() {
-        String orderByClause = SQLUtils.buildOrderByClause("couldBeAnything", null);
+        final String orderByClause = SQLUtils.buildOrderByClause("couldBeAnything", null, validSortFields);
         assertNull(orderByClause);
     }
 
     @Test
     public void setsOrderWithAscending() {
-        String orderByClause = SQLUtils.buildOrderByClause(SQLUtils.SORT_ASCENDING, "abc");
+        final String orderByClause = SQLUtils.buildOrderByClause(SQLUtils.SORT_ASCENDING, "abc", validSortFields);
         assertEquals("ORDER abc ASC", orderByClause);
     }
 
     @Test
     public void setsOrderWithDescending() {
-        String orderByClause = SQLUtils.buildOrderByClause(SQLUtils.SORT_DESCENDING, " xxx ");
+        final String orderByClause = SQLUtils.buildOrderByClause(SQLUtils.SORT_DESCENDING, " xxx ", validSortFields);
         assertEquals("ORDER xxx DESC", orderByClause);
     }
 
     @Test(expected = DMCServiceException.class)
     public void throwsExceptionIfNotAscendingOrDescendingOrder() {
-        SQLUtils.buildOrderByClause("invalidOrder", " xxx ");
+        SQLUtils.buildOrderByClause("invalidOrder", " xxx ", validSortFields);
+    }
+
+    @Test
+    public void setsOrderForMultipleFields() {
+        final String orderByClause = SQLUtils.buildOrderByClause(null, "abc, xxx", validSortFields);
+        assertEquals("ORDER abc, xxx", orderByClause);
+    }
+
+    @Test
+    public void setsOrderForMultipleFieldsWithOrder() {
+        final String orderByClause = SQLUtils.buildOrderByClause(SQLUtils.SORT_ASCENDING, "abc, xxx", validSortFields);
+        assertEquals("ORDER abc, xxx ASC", orderByClause);
+    }
+
+    @Test
+    public void setsOrderForMultipleFieldsWithOrderInSortFields() {
+        final String orderByClause = SQLUtils.buildOrderByClause(null, "abc   DESC,   xxx ASC", validSortFields);
+        assertEquals("ORDER abc DESC, xxx ASC", orderByClause);
+    }
+
+    @Test(expected = DMCServiceException.class)
+    public void throwsExceptionIfOrderSetAndInFieldSpec() {
+        SQLUtils.buildOrderByClause(SQLUtils.SORT_ASCENDING, "abc   DESC,   xxx ASC", validSortFields);
+    }
+
+    @Test(expected = DMCServiceException.class)
+    public void throwsExceptionForBadFieldWithMultipleFields() {
+        SQLUtils.buildOrderByClause(SQLUtils.SORT_ASCENDING, "def, xxx", validSortFields);
     }
 
     @Test
     public void setsStringToNullIfLimitIsNull() {
-        String limitClause = SQLUtils.buildLimitClause(null);
+        final String limitClause = SQLUtils.buildLimitClause(null);
         assertNull(limitClause);
     }
 
     @Test
     public void setsLimitString() {
-        String limitClause = SQLUtils.buildLimitClause(15);
+        final String limitClause = SQLUtils.buildLimitClause(15);
         assertEquals("LIMIT 15", limitClause);
     }
 
