388b991a3088c1180549043f094f452f6a34851d
Max Rowland <mrowland@nexient.com>
2016-08-02 09:00:13 -0400

    Adding user token entity, model, service, repository, etc.

---
+++ src/main/java/org/dmc/services/users/VerifyUserResponse.java
@@ -0,0 +1,25 @@
+package org.dmc.services.users;
+
+public class VerifyUserResponse {
+
+	Integer responseCode;
+
+	String responseDescription;
+
+	public Integer getResponseCode() {
+		return responseCode;
+	}
+
+	public void setResponseCode(Integer responseCode) {
+		this.responseCode = responseCode;
+	}
+
+	public String getResponseDescription() {
+		return responseDescription;
+	}
+
+	public void setResponseDescription(String responseDescription) {
+		this.responseDescription = responseDescription;
+	}
+
+}
