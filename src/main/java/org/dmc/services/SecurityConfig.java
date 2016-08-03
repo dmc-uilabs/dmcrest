6a9be58d5b64f005d69dd8c7d8b0be072fe83e7d
Anthony Wamsley <awamsley@nexient.com>
2016-08-03 10:51:18 -0400

    Remove spring security user caching

--- src/main/java/org/dmc/services/SecurityConfig.java
+++ src/main/java/org/dmc/services/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsByNameServiceWrapper;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
@@ -43,6 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		http
 			.csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.addFilter(authFilter)
 			.authorizeRequests().anyRequest().permitAll()
 			.and().exceptionHandling().authenticationEntryPoint(new AuthenticationExceptionHandler())
