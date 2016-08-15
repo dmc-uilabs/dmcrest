6f3721a550d6e81510cf56dd0e65c14830c9646a
Anthony Wamsley <awamsley@nexient.com>
2016-08-15 09:50:40 -0400

    Add column mappings for DMDIIProjectContact

--- src/main/java/org/dmc/services/data/entities/DMDIIProjectContact.java
+++ src/main/java/org/dmc/services/data/entities/DMDIIProjectContact.java
@@ -1,5 +1,6 @@
 package org.dmc.services.data.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,8 +15,10 @@ public class DMDIIProjectContact extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@Column(name = "first_name")
 	private String firstName;
 	
+	@Column(name = "last_name")
 	private String lastName;
 	
 	private String email;
