1120b25a0432b3e30b2f6088a51d2f6885f6a91d
Junrong <gloria.yanrr@gmail.com>
2016-08-29 19:17:46 -0400

    change userEPPN to use from baseIT

--- src/test/java/org/dmc/services/CompareServiceIT.java
+++ src/test/java/org/dmc/services/CompareServiceIT.java
@@ -11,8 +11,7 @@ public class CompareServiceIT extends BaseIT {
 	private static final String PROFILE_COMPARE_SERVICE = "/profiles/{profileID}/compare_services";
 	private static final String PROFILE_POST_COMPARE_SERVICE = "/compare_services";
 	private static final String PROFILE_DELETE_COMPARE_SERVICE = "/compare_services/{id}";
-	public static final String userEPPN = "testUser"; // 111
-	public static final String forgeAdmin = "fforgeadmin"; // 102
+	public static final String testUser = "testUser"; // 111
 
 	@Test
 	public void postAndDeleteCompareService() {
@@ -23,15 +22,14 @@ public class CompareServiceIT extends BaseIT {
 		json.setProfileId(Integer.toString(profileId));
 
 		GetCompareService compareService = given().header("Content-type", "application/json")
-				.header("AJP_eppn", forgeAdmin).body(json).expect().statusCode(HttpStatus.OK.value()).when()
+				.header("AJP_eppn", userEPPN).body(json).expect().statusCode(HttpStatus.OK.value()).when()
 				.post(PROFILE_POST_COMPARE_SERVICE).as(GetCompareService.class);
 		int id = Integer.parseInt(compareService.getId());
 		deleteCompareService(id);
 	}
 
-	// @Test
 	public void deleteCompareService(int id) {
-		given().header("Content-type", "application/json").header("AJP_eppn", forgeAdmin).expect()
+		given().header("Content-type", "application/json").header("AJP_eppn", userEPPN).expect()
 				.statusCode(HttpStatus.OK.value()).when().delete(PROFILE_DELETE_COMPARE_SERVICE, id);
 
 	}
@@ -39,7 +37,7 @@ public class CompareServiceIT extends BaseIT {
 	@Test
 	public void deleteNonExistingCompareService() {
 		int id = 1111121211;
-		given().header("Content-type", "application/json").header("AJP_eppn", forgeAdmin).expect()
+		given().header("Content-type", "application/json").header("AJP_eppn", testUser).expect()
 				.statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value()).when().delete(PROFILE_DELETE_COMPARE_SERVICE, id);
 
 	}
@@ -47,14 +45,14 @@ public class CompareServiceIT extends BaseIT {
 	@Test
 	public void getCompareServices() {
 		int profileId = 111;
-		given().header("AJP_eppn", userEPPN).expect().statusCode(HttpStatus.OK.value()).when()
+		given().header("AJP_eppn", testUser).expect().statusCode(HttpStatus.OK.value()).when()
 				.get(PROFILE_COMPARE_SERVICE, profileId);
 	}
 
 	@Test
 	public void getCompareServicesWithInvalidProfileId() {
 		int profileId = 102;
-		given().header("AJP_eppn", userEPPN).expect().statusCode(HttpStatus.UNAUTHORIZED.value()).when()
+		given().header("AJP_eppn", testUser).expect().statusCode(HttpStatus.UNAUTHORIZED.value()).when()
 				.get(PROFILE_COMPARE_SERVICE, profileId);
 	}
 
