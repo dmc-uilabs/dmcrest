521b628e0f7e86e01e5f4cc9cc08bc612341c36d
Keith Skronek <kskronek@nexient.com>
2016-08-31 17:51:53 -0400

    Changed data source from PGSimpleDataSource to PGPooledDataSource

--- src/main/java/org/dmc/services/Application.java
+++ src/main/java/org/dmc/services/Application.java
@@ -1,8 +1,6 @@
 package org.dmc.services;
 
-import javax.sql.DataSource;
-
-import org.postgresql.ds.PGSimpleDataSource;
+import org.postgresql.ds.PGPoolingDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
@@ -10,26 +8,28 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(exclude = {SolrAutoConfiguration.class})
+import javax.sql.DataSource;
+
+@SpringBootApplication(exclude = { SolrAutoConfiguration.class })
 public class Application extends SpringBootServletInitializer {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
-    }
-    @Bean
-    public DataSource getDataSource() {
-        PGSimpleDataSource ds = new PGSimpleDataSource();
-        ds.setUser(Config.DB_USER);
-        ds.setPassword(Config.DB_PASS);
-        ds.setServerName(Config.DB_IP);
-        ds.setPortNumber(Integer.parseInt(Config.DB_PORT));
-        ds.setDatabaseName(Config.DB_NAME);
-        return ds;
-    }
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
 
+	@Bean
+	public DataSource getDataSource() {
+		PGPoolingDataSource ds = new PGPoolingDataSource();
+		ds.setUser(Config.DB_USER);
+		ds.setPassword(Config.DB_PASS);
+		ds.setServerName(Config.DB_IP);
+		ds.setPortNumber(Integer.parseInt(Config.DB_PORT));
+		ds.setDatabaseName(Config.DB_NAME);
+		return ds;
+	}
 }
