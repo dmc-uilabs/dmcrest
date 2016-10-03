60075ef7f0d28315904f85174be223c71efe33fa
dmcmillen <dmcmillen@nexient.com>
2016-10-03 17:40:40 -0400

    DMC-1069/1079 Document cron jobs.
    - Adding scheduled job to remove old failed verified document records.
    - Adding scheduled job to refresh AWS documents a day before they expire (if they are still valid).

--- src/main/java/org/dmc/services/data/repositories/DocumentRepository.java
+++ src/main/java/org/dmc/services/data/repositories/DocumentRepository.java
@@ -2,7 +2,13 @@ package org.dmc.services.data.repositories;
 
 import org.dmc.services.data.entities.Document;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 public interface DocumentRepository extends BaseRepository<Document, Integer> {
 
+	List<Document> findAllByVerifiedIsFalseAndModifiedBefore(Timestamp modified);
+
+	List<Document> findAllByVerifiedIsTrueAndIsDeletedIsFalseAndExpiresBefore(Timestamp expires);
 
 }
