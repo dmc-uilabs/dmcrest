9bb3298c9f3edce126a19b53b025437853bfe89e
Max Rowland <mrowland@nexient.com>
2016-11-10 08:41:28 -0500

    Fixing delete endpoint

--- src/main/java/org/dmc/services/organization/OrganizationController.java
+++ src/main/java/org/dmc/services/organization/OrganizationController.java
@@ -1,5 +1,9 @@
 package org.dmc.services.organization;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.dmc.services.data.models.OrganizationModel;
 import org.dmc.services.exceptions.MissingIdException;
 import org.dmc.services.security.PermissionEvaluationHelper;
@@ -14,10 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.inject.Inject;
-import javax.websocket.server.PathParam;
-import java.util.List;
 
 @RestController
 public class OrganizationController {
@@ -66,7 +66,7 @@ public class OrganizationController {
 
 	@RequestMapping(value = "/organizations/{id}", method = RequestMethod.DELETE)
 	@PreAuthorize(SecurityRoles.REQUIRED_ROLE_SUPERADMIN)
-	public void delete(@PathParam("organizationId") Integer organizationId) {
-		organizationService.delete(organizationId);
+	public void delete(@PathVariable("id") Integer id) {
+		organizationService.delete(id);
 	}
 }
