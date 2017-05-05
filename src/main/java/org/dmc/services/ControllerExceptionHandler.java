f23b2f6e21c3a78f3b9bd3ed8ca7519b60a907bc
Jonathan Brand <jhbrand@umich.edu>
2017-05-05 10:36:54 -0500

    Logging raw message to logfile

--- src/main/java/org/dmc/services/ControllerExceptionHandler.java
+++ src/main/java/org/dmc/services/ControllerExceptionHandler.java
@@ -7,15 +7,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.dmc.services.ServiceLogger;
+
 import com.amazonaws.services.importexport.model.MissingParameterException;
 
 @ResponseBody
 @ControllerAdvice
 public class ControllerExceptionHandler {
+	private final String logTag = ControllerExceptionHandler.class.getName();
 
 	@ExceptionHandler(Exception.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	public ResponseEntity handleUncaughtException(Exception e) {
+		log(logTag, e.getMessage())
 		ErrorMessage error = new ErrorMessage.ErrorMessageBuilder("REST Internal Error").build();
 		return new ResponseEntity<ErrorMessage>(error, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
