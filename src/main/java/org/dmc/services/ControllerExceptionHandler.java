92b871b0f33c410e1305d32c22599bfa40645852
Jonathan Brand <jhbrand@umich.edu>
2017-05-12 14:16:03 -0500

    Logging exception using ServiceLogger.logException before returning "REST Internal Error"

--- src/main/java/org/dmc/services/ControllerExceptionHandler.java
+++ src/main/java/org/dmc/services/ControllerExceptionHandler.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.dmc.services.DMCServiceException;
 import org.dmc.services.ServiceLogger;
+import org.dmc.services.DMCError;
 
 import com.amazonaws.services.importexport.model.MissingParameterException;
 
@@ -17,9 +19,8 @@ public class ControllerExceptionHandler {
 	private final String logTag = ControllerExceptionHandler.class.getName();
 
 	@ExceptionHandler(Exception.class)
-	//@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	public ResponseEntity handleUncaughtException(Exception e) {
-		ServiceLogger.log(logTag, e.getMessage());
+		ServiceLogger.logException(logTag, new DMCServiceException(DMCError.Generic, e.getMessage()));
 		ErrorMessage error = new ErrorMessage.ErrorMessageBuilder("REST Internal Error").build();
 		return new ResponseEntity<ErrorMessage>(error, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
