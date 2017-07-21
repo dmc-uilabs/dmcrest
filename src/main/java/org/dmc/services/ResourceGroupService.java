ef59f3d489b60bee6e8ad45d0cd590c85a634209
emiliano-ponce <emiliano.ponce@integrisgp.com>
2017-07-21 16:25:45 -0500

    NullPointerException fix when deleting resource groups

--- src/main/java/org/dmc/services/ResourceGroupService.java
+++ src/main/java/org/dmc/services/ResourceGroupService.java
@@ -55,7 +55,9 @@ public class ResourceGroupService {
 
 		for(String role: roles) {
 			ResourceGroup group = resourceGroupRepository.findByParentTypeAndParentIdAndRole(parentType, parentId, role);
-			resourceGroupRepository.delete(group);
+			if(group != null) {
+				resourceGroupRepository.delete(group);
+			}
 		}
 	}
 	
