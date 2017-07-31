af3af597d72791acdca09481102100a2e47f97c2
emiliano-ponce <emiliano.ponce@integrisgp.com>
2017-07-31 09:20:27 -0500

    Added logging for Kibana

--- src/main/java/org/dmc/services/payments/PaymentsController.java
+++ src/main/java/org/dmc/services/payments/PaymentsController.java
@@ -1,5 +1,7 @@
 package org.dmc.services.payments;
 
+import org.dmc.services.ServiceLogger;
+import org.dmc.services.accounts.AccountsController;
 import org.dmc.services.data.entities.PaymentParentType;
 import org.dmc.services.data.models.OrgCreation;
 import org.dmc.services.data.models.OrganizationModel;
@@ -19,6 +21,8 @@ import com.stripe.model.Charge;
 
 @RestController
 public class PaymentsController {
+	
+	private final String logTag = PaymentsController.class.getName();
 
 	@Autowired
 	private PaymentsService paymentsService;
@@ -43,16 +47,20 @@ public class PaymentsController {
 					paymentsService.savePayment(charge, orgModel.getId(), PaymentParentType.ORGANIZATION);
 					return new ResponseEntity<PaymentsStatus>(new PaymentsStatus(charge.getStatus(), "Payment Successful!"), HttpStatus.OK);
 				} catch (StripeException e) {
+					ServiceLogger.log(logTag, "Stripe Exception Occurred: " + e.getMessage());
 					organizationService.delete(orgModel.getId());
 					return new ResponseEntity<PaymentsStatus>(new PaymentsStatus(charge.getStatus(), e.getMessage()), HttpStatus.OK);
 				} catch (DataAccessException dae) {
+					ServiceLogger.log(logTag, "Database Exception Occurred: " + dae.getMessage());
 					return new ResponseEntity<PaymentsStatus>(new PaymentsStatus(charge.getStatus(), "There was a problem in the database, please contact support."), HttpStatus.OK);
 				}
 			} else {
+				ServiceLogger.log(logTag, orgModel.getName() + " is already paid for.");
 				return new ResponseEntity<PaymentsStatus>(new PaymentsStatus(charge.getStatus(), "Organization already paid!"), HttpStatus.OK);
 			}
 		
 		} else {
+			ServiceLogger.log(logTag, "Database failed to create org named " + orgCreation.getOrganizationModel().getName());
 			return new ResponseEntity<PaymentsStatus>(new PaymentsStatus(charge.getStatus(), "Organization creation failed!"), HttpStatus.OK);
 		}
 
