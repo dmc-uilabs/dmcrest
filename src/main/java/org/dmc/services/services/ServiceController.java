330df97669a226d7c09fbc9a1d80d33806da694a
Jonathan Brand <jonathan.brand@uilabs.org>
2017-08-10 12:19:25 -0500

    new endpoint /defaultServices that returns list of services in default workspace

--- src/main/java/org/dmc/services/services/ServiceController.java
+++ src/main/java/org/dmc/services/services/ServiceController.java
@@ -83,6 +83,21 @@ public class ServiceController {
         }
     }
 
+    @RequestMapping(value = "/defaultServices", method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getDefaultServiceList(@RequestHeader(value = "AJP_eppn", defaultValue = "testUser") String userEPPN) {
+
+        ServiceLogger.log(LOGTAG, "In getDefaultServiceList as user: " + userEPPN);
+
+        try {
+          int defaultProjectId = projectController.findOrCreateDefaultProject(userEPPN);
+          return new ResponseEntity<ArrayList<Service>>(serviceDao.getServiceList(defaultProjectId), HttpStatus.OK);
+        }
+        catch (DMCServiceException e){
+          ServiceLogger.logException(LOGTAG, e);
+          return new ResponseEntity<String>(e.getMessage(), e.getHttpStatusCode());
+        }
+    }
+
     @RequestMapping(value = "/components/{componentId}/services", method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getServiceByComponentList(@PathVariable("componentId") int componentId) {
         try {
