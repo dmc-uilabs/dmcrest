d78bdca19c4ee4eab570901d6ce1cedc7afc085c
Clay Taylor <clay.taylor@e-gineering.com>
2017-09-12 16:48:29 -0400

    added save and delete methods

--- src/main/java/org/dmc/services/userfavorite/UserFavoriteDao.java
+++ src/main/java/org/dmc/services/userfavorite/UserFavoriteDao.java
@@ -18,6 +18,37 @@ public class UserFavoriteDao {
     private Connection connection = null;
     private FavoriteContentTypeDao favoriteContentTypeDao = new FavoriteContentTypeDao();
 
+    public UserFavorite getUserFavorite (Integer id) {
+
+        try {
+
+            connection = DBConnector.connection();
+            connection.setAutoCommit(false);
+
+            PreparedStatement getUserFavorite = null;
+
+            UserFavorite userFavorite = null;
+
+            String query = "SELECT user_id, content_id, content_type  FROM user_favorite WHERE id = ?";
+
+            getUserFavorite = connection.prepareStatement(query);
+            getUserFavorite.setInt(1, id);
+            final ResultSet resultSet = getUserFavorite.executeQuery();
+            connection.commit();
+
+            while (resultSet.next()) {
+                userFavorite = readUserFavoriteResultSet(resultSet);
+            }
+
+            return userFavorite;
+
+        } catch (SQLException e) {
+            ServiceLogger.log(logTag, e.getMessage());
+            throw new DMCServiceException(DMCError.OtherSQLError, "unable to get user favorite");
+        }
+
+    }
+
     public ArrayList<UserFavorite> getUserFavorites(Integer userId, Integer contentType) throws DMCServiceException {
 
         ArrayList<UserFavorite> userFavorites = new ArrayList<>();
@@ -53,6 +84,86 @@ public class UserFavoriteDao {
                 + userId);
         }
 
+    }
+
+    public UserFavorite save(UserFavorite userFavorite) throws DMCServiceException {
+        try {
+            connection = DBConnector.connection();
+            connection.setAutoCommit(false);
+
+            String query = "INSERT INTO user_favorite (user_id, content_id, content_type) "
+                    + " values (?, ?, ?)";
+
+            final PreparedStatement saveUserFavorite = connection.prepareStatement(query);
+
+            saveUserFavorite.setInt(1, userFavorite.getUserId());
+            saveUserFavorite.setInt(2, userFavorite.getContentId());
+            saveUserFavorite.setInt(3, userFavorite.getContentType().getId());
+
+            Integer newUserFavoriteId = saveUserFavorite.executeUpdate();
+            connection.commit();
+
+            return getUserFavorite(newUserFavoriteId);
+
+        } catch (SQLException e) {
+            ServiceLogger.log(logTag, e.getMessage());
+            if (null != connection) {
+                try {
+                    connection.rollback();
+                } catch (SQLException ex) {
+                    throw new DMCServiceException(DMCError.OtherSQLError, "unable to rollback: " + ex.getMessage());
+                }
+            }
+            throw new DMCServiceException(DMCError.OtherSQLError, "unable to save user favorites object");
+        } finally {
+            if (null != connection) {
+                try {
+                    connection.setAutoCommit(true);
+                } catch (Exception ex) {
+                   ServiceLogger.log(logTag, ex.getMessage());
+                }
+            }
+        }
+    }
+
+    public Integer delete(UserFavorite userFavorite) throws DMCServiceException {
+        try {
+
+            connection = DBConnector.connection();
+            connection.setAutoCommit(false);
+
+            String query = "DELETE FROM user_favorite WHERE user_id = ? AND content_id = ? AND content_type = ?";
+
+            final PreparedStatement deleteUserFavorite = connection.prepareStatement(query);
+
+            deleteUserFavorite.setInt(1, userFavorite.getUserId());
+            deleteUserFavorite.setInt(2, userFavorite.getContentId());
+            deleteUserFavorite.setInt(3, userFavorite.getContentType().getId());
+
+            Integer returnValue = deleteUserFavorite.executeUpdate();
+            connection.commit();
+
+            return returnValue;
+
+        } catch (SQLException e) {
+            ServiceLogger.log(logTag, e.getMessage());
+            if (null != connection) {
+                try {
+                    connection.rollback();
+                } catch (SQLException ex) {
+                    throw new DMCServiceException(DMCError.OtherSQLError, "unable to rollback: " + ex.getMessage());
+                }
+            }
+            throw new DMCServiceException(DMCError.OtherSQLError, "unable to delete user favorite");
+        } finally {
+            if (null != connection) {
+                try {
+                    connection.setAutoCommit(true);
+                } catch (Exception ex) {
+                    ServiceLogger.log(logTag, ex.getMessage());
+                }
+            }
+        }
     }
 
     private UserFavorite readUserFavoriteResultSet(ResultSet resultSet) throws SQLException {
